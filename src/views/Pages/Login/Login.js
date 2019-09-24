1bd7aeff720b0fa1810073796996302ec0fb936a
Fan Yansen <fan.yansen@onebyone.io>
2019-09-24 09:44:49 +0700

    Build FE Login Page
    This Login Page still have design, don't have functionality

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,60 +1,64 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import bg from '../../../assets/img/brand/bg.png'
+import logo from '../../../assets/img/brand/logo_ml_large.png'
 
 class Login extends Component {
   render() {
     return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-4">
+      <div className="app flex-row" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover'}}>
+        <Container style={{marginLeft: 0}}>
+          <Row className="justify-content h-100">
+            <Col md="5" sm="12" style={{paddingLeft: 0, paddingRight: 0}}>
+              <CardGroup className="h-100">
+                <Card className="p-4" style={{borderRadius: 0, width: ''}}>
                   <CardBody>
-                    <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
+                    <img src={logo} width="240px" className="img-fluid" />
+                    <Form style={{paddingTop: '60px'}}>
+                      <h2 style={{fontFamily: 'Nunito', color: '#388DCD'}}><b>Microlistics Web</b></h2>
+                      <p className="text-muted" style={{paddingBottom: '37px'}}>Welcome back, please login to your account.</p>
                       <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-user"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
+                        <input type="text" className="input" required="required"/>
+                        <span class="bar"></span>
+                        <label className="label"><b>Username</b></label>
                       </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                      <InputGroup className="mb-4" style={{marginTop: '29px'}}>
+                        <input type="password" className="input" required="required"/>
+                        <span class="bar"></span>
+                        <label className="label"><b>Password</b></label>
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
+                        <input className="inp-cbx" id="cbx" type="checkbox" style={{display: 'none'}}/>
+                          <label className="cbx" for="cbx">
+                            <span>
+                              <svg width="12px" height="10px" viewbox="0 0 12 10">
+                                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                              </svg>
+                            </span>
+                            <span style={{color: '#919191'}}>Remember Me</span>
+                          </label>
                         </Col>
                         <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
+                          <Button color="link" className="px-0" style={{color: '#919191', textDecoration: 'none', marginTop: '-6px'}}>Forgot password?</Button>
                         </Col>
+                      </Row>
+                      <Row style={{paddingTop: '45px'}}>
+                        <Col xs="6">
+                          <Button color="primary" className="px-4" style={{borderRadius: "33px", backgroundColor: '#388DCD', borderColor: '#388DCD', width: '135px'}}>Login</Button>
+                        </Col>
+                      </Row>
+                      <Row>
+
                       </Row>
                     </Form>
                   </CardBody>
                 </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
-                      </Link>
-                    </div>
-                  </CardBody>
-                </Card>
               </CardGroup>
+            </Col>
+            <Col md="7">
+              <div class="logo-box d-none d-sm-block"></div>   
             </Col>
           </Row>
         </Container>
