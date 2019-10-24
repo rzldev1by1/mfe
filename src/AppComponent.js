8036b977c0ff4e50b8697421c13249c5e60d4d08
Ardi Pranata <dev@onebyone.io>
2019-10-24 14:07:23 +0700

    integrate API with FE

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,8 +1,8 @@
 class AppComponent {
-	static API_CONFIG = {
-		KEY: 'X-DreamFactory-API-Key',
-		VALUE: 'e553e47a799d4805fde8b31374f1706b130b2902b5376fbba6f4817ad3c6b272'
-	}
+	// static API_CONFIG = {
+	// 	KEY: 'X-DreamFactory-API-Key',
+	// 	VALUE: 'e553e47a799d4805fde8b31374f1706b130b2902b5376fbba6f4817ad3c6b272'
+	// }
 
 	static ENDPOINT_URL = window.AppSetting.endpointUrl;
 	static BASE_URL_PROD = window.AppSetting.gatewayUrl;
@@ -16,8 +16,10 @@ class AppComponent {
 		switch (this.ENV_MODE) {
 			case this.DEVELOPMENT :
 				return this.BASE_URL_DEV + "?endpoint=" + this.ENDPOINT_URL;
+				// return this.BASE_URL_DEV;
 			case this.PRODUCTION :
 				return this.BASE_URL_PROD + "?endpoint=" + this.ENDPOINT_URL;
+				// return this.BASE_URL_PROD;
 			default :
 				break;
 		}
