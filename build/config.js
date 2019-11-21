03ef732de83a0c211a30a991e88bdc665b49aede
Ardi Pranata <dev@onebyone.io>
2019-11-21 16:32:23 +0700

    add build folder to gitignore

--- build/config.js
+++
@@ -1,5 +0,0 @@
-AppSetting = {
-	env: "DEVELOPMENT",
-	gatewayUrl: "http://apacheweb.onebyone.io:8080/request/httprequest.php",
-	endpointUrl: "http://3.19.207.202/microlisticsapi/public/",
-}
--- src/views/StockHolding/StockDetails/StockDetails.js
+++ src/views/StockHolding/StockDetails/StockDetails.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { Card, CardBody,
 		 Table, Row,
-		//  Button, ButtonDropdown,
 		 FormGroup, InputGroup,
 		 Label,
-		//  TabContent, TabPane,
 		 Nav, NavItem, NavLink
-		//  Input, InputGroup, InputGroupAddon,
-		//  DropdownItem, DropdownMenu, DropdownToggle
 } from 'reactstrap';
 
 import axios from 'axios';
 import AppComponent from '../../../AppComponent';
-import './StockDetails.css';
+import { formatDate } from '../../../AppComponent/Helper';
 import Paging from '../../General/Paging';
+
+import './StockDetails.css';
 
 class StockDetails extends Component {
 	constructor(props) {
@@ -37,14 +35,7 @@ class StockDetails extends Component {
 				{ id: "receipt_disposition", checkboxLabelText: "Disposition", tableHeaderText: "Disposition", isVisible: true, key: "" },
 				{ id: "ref3", checkboxLabelText: "Ref 3", tableHeaderText: "Ref 3", isVisible: true, key: "" },
 				{ id: "ref4", checkboxLabelText: "Ref 4", tableHeaderText: "Ref 4", isVisible: true, key: "" },
-				{ id: "weight", checkboxLabelText: "Quantity", tableHeaderText: "Quantity", isVisible: true, key: "" },
-				// { id: "packType", checkboxLabelText: "Pack Type", tableHeaderText: "Pack Type", isVisible: true, key: "" },
-				// { id: "packSize", checkboxLabelText: "Pack Size", tableHeaderText: "Pack Size", isVisible: true, key: "" },
-				// { id: "weight", checkboxLabelText: "Weight", tableHeaderText: "Weight", isVisible: true, key: "" },
-				// { id: "volume", checkboxLabelText: "Volume", tableHeaderText: "Volume", isVisible: true, key: "" },
-				// { id: "dateStatus", checkboxLabelText: "Date Status", tableHeaderText: "Date Status", isVisible: false, key: "" },
-				// { id: "ref2", checkboxLabelText: "Ref 2", tableHeaderText: "Ref 2", isVisible: true, key: "" },
-				// { id: "alert", checkboxLabelText: "Alert", tableHeaderText: "Alert", isVisible: true, key: "" }
+				{ id: "weight", checkboxLabelText: "Quantity", tableHeaderText: "Quantity", isVisible: true, key: "" }
 			],
 			foreshadowedColumn: [
 				{ id: "site", checkboxLabelText: "Site", tableHeaderText: "Site", isVisible: true, key: "site" },
@@ -60,35 +51,83 @@ class StockDetails extends Component {
 			stockDetails: [],
 			foreshadowedData: [],
 			masterResource: [],
-			UOM: []
+			UoM: []
 		}
-		// this.getLocalStorageColumn();
 	}
 	
-	getLocalStorageColumnData = () => {
-		// // let self = this;
-		// if (localStorage.getItem("columnData") && localStorage.getItem("columnData") !== "undefined") {
-		// 	let columnItem = JSON.parse(localStorage.getItem("columnData"));
-		// 	if (columnItem) { this.state.columns = columnItem };
-		// } else {
-		// 	localStorage.setItem("columnData", JSON.stringify(this.state.columns));
-		// }
+	componentDidMount() {
+		this.loadStockHolding();
+		this.loadStockDetails();
+		this.loadForeshadowed();
 	}
 
-	formatDate = (date) => {
-		var monthNames = [
-		  "January", "February", "March",
-		  "April", "May", "June", "July",
-		  "August", "September", "October",
-		  "November", "December"
-		];
-		var dateFormat = new Date(date);
-		var day = dateFormat.getDate();
-		var monthIndex = dateFormat.getMonth();
-		var year = dateFormat.getFullYear();
-	  
-		return day + ' ' + monthNames[monthIndex] + ' ' + year;
-	}
+	loadStockHolding = () => {
+		let self = this;
+		let productId = this.props.history.location.pathname.substring(20);
+		self.setState({ isLoaded: true, isSearch: true,
+						currentPage: 1,
+						startIndex: 0, lastIndex: 0,
+						totalRows: 0, maxPage: 0 });
+
+		// if (localStorage.getItem("masterResStockHolding")) {
+		// 	let masterResStockHolding = JSON.parse(localStorage.getItem("masterResStockHolding"));
+		// 	this.setState({ masterResStockHolding: masterResStockHolding });
+		// } else {
+			// let params = {'activeonly': 'N'}
+			// let endpoint = "scale/_proc/API_ProductList";
+			// axios.get(AppComponent.getBaseUrl() + endpoint, {
+			axios.get(AppComponent.getBaseUrl() + "stockholding")
+			//     params: params,
+			//     headers: {
+			//         'Content-Type': 'application/json',
+			//         'X-DreamFactory-API-Key': 'e553e47a799d4805fde8b31374f1706b130b2902b5376fbba6f4817ad3c6b272',
+			//         'X-Company-Code': Authentication.getCompanyCode(),
+			//         'X-DreamFactory-Session-Token': Authentication.getToken(),
+			//         'Accept':'application/json'
+			//     }
+			// })
+			.then(res => {
+				// res.isSuccess = true;
+				// self.setState({ isLoaded: false })
+				return res.data;
+			})
+			.catch(function (error) {
+				self.setState({ displayContent: "NOT_FOUND",
+								isLoaded: false,
+								isSearch: false });
+				if (error.response) {
+					// self.setState({ notFoundMessage: error.response.data.message })
+				}
+				return error;
+			})
+			.then(function(result) {
+				if (result.data) {
+					let respondRes = result.data;
+					let totalPage = 0;
+
+					if (respondRes.length > self.state.displayPage) {
+						totalPage = respondRes % self.state.displayPage;
+						if (totalPage > 0 && totalPage < 50) {
+							totalPage = parseInt(respondRes.length / self.state.displayPage) + 1;
+						} else {
+							totalPage = respondRes.length / self.state.displayPage;
+						}
+						self.setState({ maxPage: totalPage });
+					} else {
+						self.setState({ maxPage: 1 });
+					}
+
+					self.setState({ displayContent: "FOUND",
+									masterResStockHolding: respondRes.find(x => x.product === productId),
+									totalRows: respondRes.length });
+					
+					self.numberEventClick(self.state.currentPage);
+					localStorage.setItem("masterResStockHolding", JSON.stringify(respondRes));
+				}
+				self.setState({ isLoaded: false, isSearch: false });
+			});
+		// }
+    }
 
 	loadStockDetails = () => {
 		let self = this;
@@ -102,7 +141,7 @@ class StockDetails extends Component {
 		// } else {
 			// let params = {'activeonly': 'N'}
 			// let endpoint = "scale/_proc/API_ProductList";
-			axios.get(AppComponent.getBaseUrl() + "stockdetail/" + this.props.history.location.pathname.substring(14))
+			axios.get(AppComponent.getBaseUrl() + "stockdetail/" + this.props.history.location.pathname.substring(20))
 			// axios.get(AppComponent.getBaseUrl() + endpoint, {
 			//     params: params,
 			//     headers: {
@@ -168,7 +207,7 @@ class StockDetails extends Component {
 		// } else {
 			// let params = {'activeonly': 'N'}
 			// let endpoint = "scale/_proc/API_ProductList";
-			axios.get(AppComponent.getBaseUrl() + "foreshadowedstockbalance/" + this.props.history.location.pathname.substring(14))
+			axios.get(AppComponent.getBaseUrl() + "foreshadowedstockbalance/" + this.props.history.location.pathname.substring(20))
 			// axios.get(AppComponent.getBaseUrl() + endpoint, {
 			//     params: params,
 			//     headers: {
@@ -222,89 +261,16 @@ class StockDetails extends Component {
 		// }
     }
 
-	loadStockHolding = () => {
-		let self = this;
-		let productId = this.props.history.location.pathname.substring(14);
-		self.setState({ isLoaded: true, isSearch: true,
-						currentPage: 1,
-						startIndex: 0, lastIndex: 0,
-						totalRows: 0, maxPage: 0 });
-
-		// if (localStorage.getItem("masterResStockHolding")) {
-		// 	let masterResStockHolding = JSON.parse(localStorage.getItem("masterResStockHolding"));
-		// 	this.setState({ masterResStockHolding: masterResStockHolding });
-		// } else {
-			// let params = {'activeonly': 'N'}
-			// let endpoint = "scale/_proc/API_ProductList";
-			axios.get(AppComponent.getBaseUrl() + "stockholding")
-			// axios.get(AppComponent.getBaseUrl() + endpoint, {
-			//     params: params,
-			//     headers: {
-			//         'Content-Type': 'application/json',
-			//         'X-DreamFactory-API-Key': 'e553e47a799d4805fde8b31374f1706b130b2902b5376fbba6f4817ad3c6b272',
-			//         'X-Company-Code': Authentication.getCompanyCode(),
-			//         'X-DreamFactory-Session-Token': Authentication.getToken(),
-			//         'Accept':'application/json'
-			//     }
-			// })
-			.then(res => {
-				// res.isSuccess = true;
-				// self.setState({ isLoaded: false })
-				return res.data;
-			})
-			.catch(function (error) {
-				self.setState({ displayContent: "NOT_FOUND",
-								isLoaded: false,
-								isSearch: false });
-				if (error.response) {
-					// self.setState({ notFoundMessage: error.response.data.message })
-				}
-				return error;
-			})
-			.then(function(result) {
-				if (result.data) {
-					let respondRes = result.data;
-					let totalPage = 0;
-
-					if (respondRes.length > self.state.displayPage) {
-						totalPage = respondRes % self.state.displayPage;
-						if (totalPage > 0 && totalPage < 50) {
-							totalPage = parseInt(respondRes.length / self.state.displayPage) + 1;
-						} else {
-							totalPage = respondRes.length / self.state.displayPage;
-						}
-						self.setState({ maxPage: totalPage });
-					} else {
-						self.setState({ maxPage: 1 });
-					}
-
-					self.setState({ displayContent: "FOUND",
-									masterResStockHolding: respondRes.find(x => x.product === productId),
-									totalRows: respondRes.length });
-					
-
-					self.numberEventClick(self.state.currentPage);
-					localStorage.setItem("masterResStockHolding", JSON.stringify(respondRes));
-				}
-				self.setState({ isLoaded: false, isSearch: false });
-			});
-		// }
-    }
-
 	updateTableColumn = (columns) => {
 		// // let self = this;
 		// this.setState({ columns: columns });
 		// localStorage.setItem("columnData", JSON.stringify(this.state.columns));
 	}
 
-	componentDidMount() {
-		this.loadStockDetails();
-		this.loadStockHolding();
-		this.loadForeshadowed();
-	}
-
 	toggleDisplayMoreColumn = () => {
-		this.setState({ displayMoreColumnModal: !this.state.displayMoreColumnModal });
+		this.setState((prevState) => { 
+			return { displayMoreColumnModal: !prevState.displayMoreColumnModal }
+		});
 	}
 
 	activeTabIndex = (tabIndex) => {
@@ -354,9 +320,9 @@ class StockDetails extends Component {
 						if (item.id === "qty" ||
 							item.id === "weight" ||
 							item.id === "volume") {
-							return <th className="p-3 text-right align-middle" key={idx} width="15%">{item.tableHeaderText}</th>
+							return <th className="p-3 text-right" key={idx} style={{ width: "auto" }}>{item.tableHeaderText}</th>
 						}
-						return <th className="p-3 text-left align-middle" key={idx} width="15%">{item.tableHeaderText}</th>
+						return <th className="p-3 text-left" key={idx} style={{ width: "auto" }}>{item.tableHeaderText}</th>
 					}
 				})}
 			</tr>
@@ -372,11 +338,11 @@ class StockDetails extends Component {
 							if (column.id === "qty" ||
 								column.id === "weight" ||
 								column.id === "volume") {
-								return <td key={columnIdx} className="px-3 text-right" width="15%">{item[column.id]}</td>
+								return <td key={columnIdx} className="px-3 text-right" style={{ width: "auto" }}>{item[column.id]}</td>
 							}
 							return (
-								<td key={columnIdx} className="px-3 text-left" width="15%">
-									{column.id === "effective_date" ? this.formatDate(item[column.id]) : item[column.id]}
+								<td key={columnIdx} className="px-3 text-left" style={{ width: "auto" }}>
+									{column.id === "effective_date" ? formatDate(item[column.id]) : item[column.id]}
 								</td>
 							)
 						}
@@ -394,9 +360,9 @@ class StockDetails extends Component {
 						if (item.id === "qty" ||
 							item.id === "weight" ||
 							item.id === "volume") {
-							return <th className="p-3 text-right align-middle" key={idx} width="10%">{item.tableHeaderText}</th>
+							return <th className="p-3 text-right" key={idx} width="10%">{item.tableHeaderText}</th>
 						}
-						return <th className="p-3 text-left align-middle" key={idx} width="10%">{item.tableHeaderText}</th>
+						return <th className="p-3 text-left" key={idx} width="10%">{item.tableHeaderText}</th>
 					}
 				})}
 			</tr>
@@ -417,7 +383,7 @@ class StockDetails extends Component {
 
 							return (
 								<td key={columnIdx} className="px-3 text-left" width="10%">
-									{column.id === "sm_dtm" ? this.formatDate(item[column.id]) : item[column.id]}
+									{column.id === "sm_dtm" ? formatDate(item[column.id]) : item[column.id]}
 								</td>
 							)
 						}
@@ -431,39 +397,53 @@ class StockDetails extends Component {
 		let content;
 		let stockHoldingTable;
 		let foreshadowedTable;
-		this.state.stockDetails.map(item => (this.state.UOM.push(item["packdesc_2"])));
+		this.state.stockDetails.map(item => (this.state.UoM.push(item["packdesc_2"])));
 
-		stockHoldingTable = 
-			<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
-				<Table className="table-condensed table-responsive table-striped rounded-bottom-175 mb-0" size="lg">
-					<thead>{this.showStockDetailsHeader()}</thead>
-					<tbody>{this.showStockDetailsData()}</tbody>
-				</Table>
+		if (this.state.stockDetails) {
+			stockHoldingTable = 
+				<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
+					<Table className="table-condensed table-responsive table-striped rounded-bottom-175 mb-0" size="xl" width="100%">
+						<thead>{this.showStockDetailsHeader()}</thead>
+						<tbody>{this.showStockDetailsData()}</tbody>
+					</Table>
 
-				<div className="bg-transparent card-footer text-center border-company border-top-0">
-					<Paging backPageClick={this.backPageClick} nextPageClick={this.nextPageClick}
-							totalRows={this.state.totalRows} displayPage={this.state.displayPage}
-							currentPage={this.state.currentPage} maxPage={this.state.maxPage}
-							isActive={this.state.isActive}
-							numberEventClick={this.numberEventClick} />
+					<div className="bg-transparent card-footer text-center border-company border-top-0">
+						<Paging backPageClick={this.backPageClick} nextPageClick={this.nextPageClick}
+								totalRows={this.state.totalRows} displayPage={this.state.displayPage}
+								currentPage={this.state.currentPage} maxPage={this.state.maxPage}
+								isActive={this.state.isActive}
+								numberEventClick={this.numberEventClick} />
+					</div>
 				</div>
-			</div>
-
-		foreshadowedTable = 
-			<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
-				<Table className="table-condensed table-responsive table-striped rounded-bottom-175 mb-0" size="lg">
-					<thead>{this.showForeshadowedHeader()}</thead>
-					<tbody>{this.showForeshadowedData()}</tbody>
-				</Table>
-
-				<div className="bg-transparent card-footer text-center border-company border-top-0">
-					<Paging backPageClick={this.backPageClick} nextPageClick={this.nextPageClick}
-							totalRows={this.state.totalRows} displayPage={this.state.displayPage}
-							currentPage={this.state.currentPage} maxPage={this.state.maxPage}
-							isActive={this.state.isActive}
-							numberEventClick={this.numberEventClick} />
+		} else {
+			stockHoldingTable =
+				<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
+					<span>No data found</span>
 				</div>
-			</div>
+		}
+
+		if (this.state.foreshadowedData) {
+			foreshadowedTable = 
+				<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
+					<Table className="table-condensed table-responsive table-striped rounded-bottom-175 mb-0" size="lg">
+						<thead>{this.showForeshadowedHeader()}</thead>
+						<tbody>{this.showForeshadowedData()}</tbody>
+					</Table>
+
+					<div className="bg-transparent card-footer text-center border-company border-top-0">
+						<Paging backPageClick={this.backPageClick} nextPageClick={this.nextPageClick}
+								totalRows={this.state.totalRows} displayPage={this.state.displayPage}
+								currentPage={this.state.currentPage} maxPage={this.state.maxPage}
+								isActive={this.state.isActive}
+								numberEventClick={this.numberEventClick} />
+					</div>
+				</div>
+		} else {
+			foreshadowedTable =
+				<div className="col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 pl-0">
+					<span>No data found</span>
+				</div>
+		}
 
 		switch (this.state.displayContent) {
 			case "FOUND" :
@@ -538,7 +518,7 @@ class StockDetails extends Component {
 																					<Label className="font-weight-bold primary-text">UoM</Label>
 																				</div>
 																				<div className="col-3">
-																					<Label className="secondary-text">{this.state.UOM[0]}</Label>
+																					<Label className="secondary-text">{this.state.UoM[0]}</Label>
 																				</div>
 																				
 																				<div className="col-3">
@@ -645,7 +625,7 @@ class StockDetails extends Component {
 					</div>
 				</div>
 		}
-		return(
+		return (
 			<React.Fragment>
 				{content}
 			</React.Fragment>
