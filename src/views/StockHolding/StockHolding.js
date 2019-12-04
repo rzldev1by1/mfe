341f7eac3662196ed023fcf67103a08d801893b8
adhiyatna <adhiyatna@onebyone.io>
2019-12-04 16:00:58 +0700

    update modal design

--- src/views/StockHolding/StockHolding.js
+++ src/views/StockHolding/StockHolding.js
@@ -4,7 +4,7 @@ import { Card, CardBody,
 		 Button,
 		 ButtonDropdown,
 		 FormGroup,
-		 
+
 		Breadcrumb, BreadcrumbItem,
          Input, InputGroup, InputGroupAddon,
          DropdownItem, DropdownMenu, DropdownToggle
