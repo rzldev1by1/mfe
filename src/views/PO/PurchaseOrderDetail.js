d7c185ec58581a4ccbdc991241ac4f9d9eec4158
adhiyatna <adhiyatna@onebyone.io>
2019-12-11 14:20:05 +0700

    PurchaseOrderDetail

---
+++ src/views/PO/PurchaseOrderDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Card, CardBody,Col, Row, Table,Button,ButtonDropdown,FormGroup,
+		Breadcrumb, BreadcrumbItem,Input, InputGroup, InputGroupAddon,
+    DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
+
+import axios from 'axios';
+import AppComponent from '../../AppComponent';
+import Paging from '../General/Paging';
+
+
+class PurchaseOrderDetail extends Component{
+
+}
+
+export default PurchaseOrderDetail;
