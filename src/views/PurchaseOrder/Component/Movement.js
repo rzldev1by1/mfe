d40d353a4ad4c6b1b3aae529bfb407df70411169
briancapah <brian.capah@onebyone.io>
2020-02-07 14:10:53 +0700

    stock movement

--- src/views/PurchaseOrder/Component/Movement.js
+++ src/views/PurchaseOrder/Component/Movement.js
@@ -2,19 +2,23 @@ import React, {Component} from 'react'
 import {Container, Row, Col} from 'reactstrap'
 import axios from 'axios';
 import {endpoint, headers} from '../../../AppComponent/ConfigEndpoint'
+import moment from 'moment';
 class Movement extends Component {
     constructor(props){
         super(props)
 
         this.state = {
-            data:[],
+            data:[],            
             startDate:'2019-07-15',
-            endDate:'2019-09-30'
+            endDate:'2019-09-30',
+            dateArray:[],
+            dataArray:[]
         }
     }
 
     componentDidMount(){
         this.getData()
+        this.pushTable()
     }
 
     getData = () => {
@@ -31,7 +35,6 @@ class Movement extends Component {
     }
 
     tableMovement = (props) => {
-        console.log(props.data)
         return(
         <div style={{display:'flex'}}>
             <div className='tet' xs='2'>{props.detail.sa_plus ? props.detail.sa_plus : '-'}</div>
@@ -42,11 +45,90 @@ class Movement extends Component {
         )
     }
 
-    render(){
+    pushTable = () => {
+        let dateArray = ['2019-01-15']
+        let startDate = moment('2019-01-15')
+        let endDate = moment('2019-09-30')
+        let startMonth = startDate.format('M')
+        let endMonth = endDate.format('M')
+        while(startMonth < endMonth)
+        {
+            startDate.add(1, 'M')          
+            let newDate = startDate.format('YYYY-MM-DD')
+            dateArray.push(newDate)
+            startMonth++
+        }
+        this.setState({dateArray:dateArray})
+    }
+
+    pushData = () => {
+        let data = this.state.data
+        for(let i = 0 ; i < data.length ; i ++)
+        {
+            let availableDate = []
+            for(let x = 0 ; x < data[i].detail.length ; x++)
+            {
+                availableDate.push(data[i].detail[x].date)
+            }
+
+            for(let y = 0 ; y < this.state.dateArray.length ; y ++)
+            {
+                if(!availableDate.includes(this.state.dateArray[y]))
+                {
+                    let dataFormat =  {
+                        'date':this.state.dateArray[y], 
+                        'sa_plus':'-',  
+                        'sa_minus':'-',  
+                        'recv_weight':'-', 
+                        'send_weight':'-'
+                    }
+                    data[i].detail.push(dataFormat)
+                }
+            }
+        }
+    }
+
+    sortData = () => {
+        let data = this.state.data
+
+        for(let i = 0 ; i < data.length ; i ++)
+        {
+            let detail = data[i].detail
+
+            detail.sort((a,b) => {
+                if(a['date'] !== null && b['date'] !== null)
+                {
+                  if(a['date'].toLowerCase() < b['date'].toLowerCase()) return -1
+                  if(a['date'].toLowerCase() > b['date'].toLowerCase()) return 1
+                  return 0
+                }
+              })
+        }
         console.log(this.state.data)
+    }
+
+    movementHeader = (date) => {
+        return(
+        <div>
+            <div>{date}</div>
+            <div style={{display:'flex'}}>
+            <div className='tet' xs='2'>SA+</div>
+            <div className='tet' xs='2'>SA-</div>
+            <div className='tet' xs='2'>Rec</div>
+            <div className='tet' xs='3'>Send</div>
+        </div>
+        </div>
+        )
+    }
+
+    render(){
+      
         return(
             <Container className="themed-container conts" fluid={true}>
+              
                <Col className='cont scrollx' style={{display:'flex'}}>
+               { this.pushData()}
+               { this.sortData()}
                 {/* <Col xs='4'>
                     <Row>
                         <Col xs='1' className='tablez'>Site</Col>
@@ -84,6 +166,11 @@ class Movement extends Component {
                             <td rowSpan='2'>Product</td>
                             <td rowSpan='2'>Description</td>
                             <td rowSpan='2'>UOM</td>
+                            { 
+                                this.state.dateArray.map(date =>
+                                    <td>{this.movementHeader(date)}</td>
+                                        )
+                            }
                         </tr>
                     </thead>
 
