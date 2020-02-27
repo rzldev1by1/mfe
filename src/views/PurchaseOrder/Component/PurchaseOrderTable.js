7a7dd13e04568fd3877d46697d16b6f049cd7837
Fan Yansen <fan.yansen@onebyone.io>
2020-02-27 11:28:01 +0700

    Change First Page and Last Page Icon

--- src/views/PurchaseOrder/Component/PurchaseOrderTable.js
+++ src/views/PurchaseOrder/Component/PurchaseOrderTable.js
@@ -24,7 +24,7 @@ class PurchaseOrderTable extends Component {
       currentPage: 1,
 			startIndex: 0,
 			lastIndex: 0,
-			displayPage: 30,
+			displayPage: 15,
 			totalRows: 0,
 			maxPage: 0,
     }
@@ -270,6 +270,16 @@ class PurchaseOrderTable extends Component {
         }
         return;
 	}
+
+  firstPageClick = () => {
+    if (this.state.currentPage > 1) {
+        let currentPage = parseInt(1);
+        this.setState({ currentPage: currentPage});
+        this.changeStartIndex(currentPage);
+        this.changeLastIndex(currentPage);
+    }
+    return;
+}
 
     lastPageClick = () => {
         if (this.state.currentPage < this.state.maxPage) {
