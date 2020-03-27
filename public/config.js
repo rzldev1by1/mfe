50a3f45f1acbfe4485f39db861876093337099dc
briancapah <brian.capah@onebyone.io>
2020-03-27 10:49:12 +0700

    update api url

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 AppSetting = {
 	env: "DEVELOPMENT",
 	gatewayUrl: "http://apacheweb.onebyone.io:8080/request/httprequest.php",
-	endpointUrl: "http://54.79.96.193/microlisticsapi/public/",
+	endpointUrl: "http://api.mls.onebyone.io:81/",
 }
