f785244a2df0d58d862d88521c683a682bce5105
hanjar <hanjar.muhammad@onebyone.io>
2020-04-01 15:41:15 +0700

    update Export

--- src/AppComponent/Export.js
+++ src/AppComponent/Export.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { InputGroup } from 'reactstrap'
 import './Export.css';
+import ExportExl from 'react-html-table-to-excel'
 
 class Export extends Component {
 	constructor(props) {
@@ -10,15 +11,16 @@ class Export extends Component {
             exportExpand: false,
             value: ""
         };
-	}
+    }
+    
     render = () => {
         return (
             // <div className={this.props.maxPage > 1 ? "card-footer text-left border-company border-top-0 pl-0 pr-0 bg-transparent" : "d-none"}>
-            <div className={"card-footer text-left border-company border-top-0 pl-0 pr-0 bg-transparent"}>               
+            <div className={"card-footer text-left border-company border-top-0 pl-0 pr-0 bg-transparent"} style={{marginTop:"-20px"}}>               
                  <ul className={"select-export" + (this.state.exportExpand ? " expand-export" : "")} id="select">
                     <li className="expand-style-export">
                         <input className="select_close-export" type="radio" name="export" id="export-btn-close" value="" />
-                        <span className="select_label-export select_label-placeholder-export">Export</span>
+                        <span className="select_label-export1 select_label-placeholder-export">Export</span>
                     </li>
                 
                     <li className="select_items-export">
@@ -28,13 +30,15 @@ class Export extends Component {
                             <li className="select_option-export">
                                 <input className="select_input-export" type="radio" name="export" />
                                 <label className="select_label-export" htmlFor="Export to PDF">
-                                    <span className="pdf-icon">Export to PDF</span>
+                                    <span className="pdf-icon"onClick={this.ExportPDF} >Export to PDF</span>
                                 </label>
                             </li>
                             <li className="select_option-export">
                                 <input className="select_input-export" type="radio" name="export" />
                                 <label className="select_label-export option_radius-export" htmlFor="Export to XLS">
-                                    <span className="excel-icon">Export to XLS</span>
+                                    <span className="excel-icon" >
+                                        <ExportExl className="btn excel" table="excel" filename="Report" sheet="sheet 4"buttonText="Export to XLS"/>
+                                    </span>
                                 </label>
                             </li>
                         </ul>
