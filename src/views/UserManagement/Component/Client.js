2dbe9356ec20d6ad9922034ca79fe2b4d261b2d2
adhiyatna <adhiyatna@onebyone.io>
2020-05-26 10:38:53 +0700

    update UM

--- src/views/UserManagement/Component/Client.js
+++ src/views/UserManagement/Component/Client.js
@@ -10,15 +10,18 @@ const client = ({clients, onEnableClick, onClientEnableAll, isEnableAllClient}) 
 return (<div>
 
       <div className="flex-column">
-          <div className="row mr-2">
+          <div className="d-flex mr-2">
               <label className="col-6 title-label p-0" htmlFor="Client">Client
               </label>
               <input className="checkbox-hidden" type="checkbox" id="Client"/>
               <div className="col-3">
-<button type="button" className={`btn mb-2 ${isEnableAllClient?'btn-outline-All-notActive':'btn-outline-All-active'} `} onClick={onClientEnableAll}>{(isEnableAllClient)?"Disable All":"Enabled All"}</button>
+                    <button type="button" className={`btn mb-2 ${isEnableAllClient?'btn-outline-All-notActive':'btn-outline-All-active'} `} onClick={onClientEnableAll}>{(isEnableAllClient)?"Disable All":"Enabled All"}</button>
+              </div>
+              <div className="col-3">
+
               </div>
           </div>
-          <div className="row mb-2">
+          <div className="d-flex mb-2">
               <div className="col-6 line-border"></div>
               <div className="col-3 line-border"></div>
             
@@ -27,7 +30,7 @@ return (<div>
       {
           clients.map((item,index) => {
             return (<div className="flex-column" key={index}>
-                          <div className="row mb-3 mr-2" key={index}>
+                          <div className="d-flex mb-3 mr-2" key={index}>
                               <label className="col-6 section-value-text p-0" key={item.code}>{ capitalizeFirstLetter(item.name) }</label>
                              
                               <input className="checkbox-hidden" type="checkbox" id={item.code} />
--- src/views/UserManagement/Component/Site.js
+++ src/views/UserManagement/Component/Site.js
@@ -5,15 +5,18 @@ const site = ({sites, onEnableClick, onSiteEnableAll, isEnableAllSite}) => {
 
 return (<div>
         <div className="flex-column mr-2">
-            <div className="row">
+            <div className="d-flex">
                 <label className="col-6 title-label" htmlFor="Site">Site
                 </label>
                 <input className="checkbox-hidden" type="checkbox" id="Site"/>
                 <div className="col-3">
                   <button type="button" className={`btn mb-2 ${isEnableAllSite?'btn-outline-All-notActive':'btn-outline-All-active'} `} onClick={onSiteEnableAll}>{(isEnableAllSite)?"Disable All":"Enabled All"}</button>
                 </div>
+                <div className="col-3">
+
+                </div>
             </div>
-            <div className="row mb-2">
+            <div className="d-flex mb-2">
               <div className="col-6 line-border ml-3"></div>
               <div className="col-3 line-border"></div>
             </div>
@@ -23,7 +26,7 @@ return (<div>
           {
             sites.map((item,index) => {
             return (<div className="flex-column mb-3 mr-2" key={index}>
-            <div className="row" key={index}>
+            <div className="d-flex" key={index}>
                 <label className="col-6 section-value-text" key={item.site}>{item.site}</label>
                 
                 <input className="checkbox-hidden" type="checkbox" id={item.site} />
