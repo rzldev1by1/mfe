11237b37465356ea65c989b0e4c8fcdce93a9013
brian capah <brian.capah@onebyone.io>
2020-05-29 16:26:15 +0700

    create forgot password form

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -14,7 +14,8 @@ class Logins extends Component {
         emailValidation:true,
         errorMessage: "",
         isLoad: false,
-        forgotPassword:false
+        forgotPassword:false,
+        policy:false
     }
 
     validateForm = (e) => {
@@ -69,6 +70,18 @@ class Logins extends Component {
 
     changeFormHanlder = () => {
         this.setState({forgotPassword: !this.state.forgotPassword,errorMessage:'', formValidation:{ username: true, password: true }, emailValidation:true})
+    }
+    
+    changePolicyHandler = () => {
+        this.setState({policy: true, forgotPassword:false})
+    }
+
+    changeTermHandler = () => {
+        this.setState({policy: true, forgotPassword:true})        
+    }
+
+    exitPolicyHandler = () => {
+        this.setState({policy: false, forgotPassword:false})
     }
 
     loginForm(errorMessage, formValidation){
@@ -126,33 +139,72 @@ class Logins extends Component {
         )
     }
 
+    termAndCondition(){
+        return(
+            <div className='privacy-and-term'>
+                <div className='policy-title inputLogin'>TERM AND CONDITIONS</div>
+                <div className='form-control text-area-policy inputLogin'> GDPR privacy policy requirements
+                    Article 12 of the GDPR requires that you communicate information about your processing of personal data in a way that is:
+
+                    concise
+                    transparent
+                    in clear and plain language
+                    intelligible
+                    easily accessible
+                    free of charge
+                    In general, most privacy laws require you to inform your users about the following:
+
+                    Your name (or business name), location, and contact information
+                    What information you’re collecting from them (including names, email addresses, IP addresses, and any other information)
+                    What methods you are using to collect their information, e.g. cookies
+                    The purpose for collecting this information
+                    How you’re keeping their information safe
+                    Whether or not it’s optional for them to share that information, how they can opt-out, and the consequences of doing so
+                    Any third-party services you’re using to collect, process, or store that information (such as an e-mail newsletter service, or advertising network)
+                    Following a GDPR privacy policy templates like the above can be a help on the way, but using a GDPR 
+                    privacy policy generator (as we link to below) can be dangerous.
+                    You must be mindful of getting all of the relevant and required information about your website into your GDPR privacy policy.
+                </div>
+
+                <div className="col-sm-4 policy-button">
+                    <button onClick={() => this.exitPolicyHandler()} type="button" className="btn btn-primary btn-login col-12">
+                        {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Back"}
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
     privacyAndPolicy(){
         return(
             <div className='privacy-and-term'>
-                GDPR privacy policy requirements
-                Article 12 of the GDPR requires that you communicate information about your processing of personal data in a way that is:
+                <div className='policy-title inputLogin'>PRIVACY AND POLICY</div>
+                <div className='form-control text-area-policy inputLogin'> GDPR privacy policy requirements
+                    Article 12 of the GDPR requires that you communicate information about your processing of personal data in a way that is:
 
-                concise
-                transparent
-                in clear and plain language
-                intelligible
-                easily accessible
-                free of charge
-                In general, most privacy laws require you to inform your users about the following:
+                    concise
+                    transparent
+                    in clear and plain language
+                    intelligible
+                    easily accessible
+                    free of charge
+                    In general, most privacy laws require you to inform your users about the following:
 
-                Your name (or business name), location, and contact information
-                What information you’re collecting from them (including names, email addresses, IP addresses, and any other information)
-                What methods you are using to collect their information, e.g. cookies
-                The purpose for collecting this information
-                How you’re keeping their information safe
-                Whether or not it’s optional for them to share that information, how they can opt-out, and the consequences of doing so
-                Any third-party services you’re using to collect, process, or store that information (such as an e-mail newsletter service, or advertising network)
-                Following a GDPR privacy policy templates like the above can be a help on the way, but using a GDPR privacy policy generator (as we link to below) can be dangerous. You must be mindful of getting all of the relevant and required information about your website into your GDPR privacy policy.
+                    Your name (or business name), location, and contact information
+                    What information you’re collecting from them (including names, email addresses, IP addresses, and any other information)
+                    What methods you are using to collect their information, e.g. cookies
+                    The purpose for collecting this information
+                    How you’re keeping their information safe
+                    Whether or not it’s optional for them to share that information, how they can opt-out, and the consequences of doing so
+                    Any third-party services you’re using to collect, process, or store that information (such as an e-mail newsletter service, or advertising network)
+                    Following a GDPR privacy policy templates like the above can be a help on the way, but using a GDPR 
+                    privacy policy generator (as we link to below) can be dangerous.
+                    You must be mindful of getting all of the relevant and required information about your website into your GDPR privacy policy.
+                </div>
 
-
-                <div className="col-sm-4 white-space">
-                    <button type="submit" className="btn btn-primary btn-login col-12">
-                        {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Send"}
+                <div className="col-sm-4 policy-button">
+                    <button onClick={() => this.exitPolicyHandler()} type="button" className="btn btn-primary btn-login col-12">
+                        {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Back"}
                     </button>
                 </div>
             </div>
@@ -160,8 +212,13 @@ class Logins extends Component {
     }
 
     render() {
-        const { errorMessage, formValidation } = this.state
+        const { errorMessage, formValidation, forgotPassword, policy } = this.state
         
+        let formComponent = this.loginForm(errorMessage, formValidation)
+        if(forgotPassword && !policy) formComponent     = this.forgotPasswordForm(errorMessage, formValidation)
+        if(!forgotPassword && !policy) formComponent    = this.loginForm(errorMessage, formValidation)
+        if(!forgotPassword && policy) formComponent     = this.privacyAndPolicy()
+        if(forgotPassword && policy) formComponent     = this.termAndCondition()
         return (
             <div className="login">
                 <div className="container-fluid">
@@ -169,13 +226,13 @@ class Logins extends Component {
                             <div className="card-body">
                                 <img src={Logo} className="logo mb-5" alt="mlslogo" />
                                     {
-                                    
-                                    this.state.forgotPassword ? this.forgotPasswordForm(errorMessage, formValidation) : this.loginForm(errorMessage, formValidation)
+                                        formComponent
+                                    // this.state.forgotPassword ? this.forgotPasswordForm(errorMessage, formValidation) : this.loginForm(errorMessage, formValidation)
                                     }
                                 <div className="links mt-3">
-                                    <span className='term-and-condition'>Privacy and Policy</span>
+                                    <span onClick={() => this.changePolicyHandler()} className='term-and-condition'>Privacy and Policy</span>
                                     <span> &nbsp; | &nbsp; </span>
-                                    <span className='term-and-condition'>Terms and Conditions</span>
+                                    <span onClick={() => this.changeTermHandler()} className='term-and-condition'>Terms and Conditions</span>
                                 </div>
                             </div>
                         </div>          
