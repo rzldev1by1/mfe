5975af523f7f14c8098e652a04739352e4d933b7
brian capah <brian.capah@onebyone.io>
2020-05-29 15:22:06 +0700

    create forgot password form

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -68,100 +68,123 @@ class Logins extends Component {
     }
 
     changeFormHanlder = () => {
-        this.setState({forgotPassword: !this.state.forgotPassword})
+        this.setState({forgotPassword: !this.state.forgotPassword,errorMessage:'', formValidation:{ username: true, password: true }, emailValidation:true})
     }
 
     loginForm(errorMessage, formValidation){
         return(
-            <form className={"offset-lg-1 " + (this.state.forgotPassword ? 'form-hidden' : 'form-show')} onSubmit={this.validateForm}>
-                        <div className="card">
-                            <div className="card-body">
-                                <img src={Logo} className="logo mb-5" alt="mlslogo" />
-                                <input className={'form-control  inputLogin ' + (formValidation.username ? "" : " is-invalid")}
-                                    type="text" name="username"
-                                    placeholder="Enter your username here" />
-                                <br />
-                                <input className={'form-control inputLogin ' + (formValidation.password ? "" : " is-invalid")}
-                                    type="password" name="password"
-                                    placeholder="Enter your password here"
-                                />
-                                <div className={'error ' + (errorMessage ? ' alertFadeIn' : '')}>
-                                    {errorMessage && <div><span className="iconU-i" /> {errorMessage}</div>}
-                                </div>
-                                <div className="row">
-                                    <div className="col-sm-4">
-                                        <button type="submit" className="btn btn-primary btn-login col-12">
-                                            {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin" /> : "Login"}
-                                        </button>
-                                    </div>
-                                    <div className="col-sm-8 mt-3">
-                                        <span className='form-login-change' onClick={() => this.changeFormHanlder()}>Forgot Password ?</span>
-                                    </div>
-                                </div>
-                                <div className="links mt-3">
-                                    <a href="#privacy">Privacy and Policy</a>
-                                    <span> &nbsp; | &nbsp; </span>
-                                    <a href="#terms">Terms and Conditions</a>
-                                </div>
+            <form className={"" + (this.state.forgotPassword ? 'form-hidden' : 'form-show')} onSubmit={this.validateForm}>
+                        <input className={'form-control  inputLogin ' + (formValidation.username ? "" : " is-invalid")}
+                            type="text" name="username"
+                            placeholder="Enter your username here" />
+                        <br />
+                        <input className={'form-control inputLogin ' + (formValidation.password ? "" : " is-invalid")}
+                            type="password" name="password"
+                            placeholder="Enter your password here"
+                        />
+                        <div className={'error ' + (errorMessage ? ' alertFadeIn' : '')}>
+                            {errorMessage && <div><span className="iconU-i" /> {errorMessage}</div>}
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <button type="submit" className="btn btn-primary btn-login col-12">
+                                    {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin" /> : "Login"}
+                                </button>
                             </div>
-                        </div>
-                        <div className="copyright offset-lg-1 mt-5">
-                            <a target='blank' href='https://www.microlistics.com.au/'>© Microlistics {new Date().getFullYear()}</a>
-                        </div>
-                    </form>
+                            <div className="col-sm-8 mt-3">
+                                <span className='form-login-change' onClick={() => this.changeFormHanlder()}>Forgot Password ?</span>
+                            </div>
+                        </div>                  
+            </form>
         )
     }
 
     forgotPasswordForm(errorMessage, formValidation){
         return(
-            <form className={"offset-lg-1 " + (this.state.forgotPassword ? 'form-show' : 'form-hidden')} onSubmit={this.validateForm}>
-                        <div className="card">
-                            <div className="card-body">
-                                <img src={Logo} className="logo mb-5" alt="mlslogo" />
-                                <input className={'form-control  inputLogin ' + (this.state.emailValidation ? "" : " is-invalid")}
-                                    type="text" name="email"
-                                    placeholder="Enter your email address here" />
-                                    <span className='email-message'>Enter your email address to find your acccount</span>
-                                    <input className={'form-control  inputLogin form-hidden '}
-                                    type="text" name="none"
-                                    placeholder="Enter your email address here" />
-                                <div className={'error ' + (errorMessage ? ' alertFadeIn' : '')}>
-                                    {errorMessage && <div><span className="iconU-i" /> {errorMessage}</div>}
-                                </div>
-                                <div className="row"> 
-                                    <div className="col-sm-4 white-space">
-                                        <button type="submit" className="btn btn-primary btn-login col-12">
-                                            {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Send"}
-                                        </button>
-                                    </div>
-                                    <div className="col-sm-8 mt-3">
-                                        <span className='form-login-change' onClick={() => this.changeFormHanlder()}>Login page</span>
-                                    </div>
-                                </div>
-                                <div className="links mt-3">
-                                    <a href="#privacy">Privacy and Policy</a>
-                                    <span> &nbsp; | &nbsp; </span>
-                                    <a href="#terms">Terms and Conditions</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="copyright offset-lg-1 mt-5">
-                            <a target='blank' href='https://www.microlistics.com.au/'>© Microlistics {new Date().getFullYear()}</a>
-                        </div>
-                    </form>
+            <form className={"" + (this.state.forgotPassword ? 'form-show' : 'form-hidden')} onSubmit={this.validateForm}>
+                <input className={'form-control  inputLogin ' + (this.state.emailValidation ? "" : " is-invalid")}
+                    type="text" name="email"
+                    placeholder="Enter your email address here" />
+                    <span className='email-message'>Enter your email address to find your acccount</span>
+                    <input className={'form-control  inputLogin form-hidden '}
+                    type="text" name="none"
+                    placeholder="Enter your email address here" />
+                <div className={'error ' + (errorMessage ? ' alertFadeIn' : '')}>
+                    {errorMessage && <div><span className="iconU-i" /> {errorMessage}</div>}
+                </div>
+                <div className="row"> 
+                    <div className="col-sm-4 white-space">
+                        <button type="submit" className="btn btn-primary btn-login col-12">
+                            {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Send"}
+                        </button>
+                    </div>
+                    <div className="col-sm-8 mt-3">
+                        <span className='form-login-change' onClick={() => this.changeFormHanlder()}>Login page</span>
+                    </div>
+                </div>    
+            </form>
+        )
+    }
+
+    privacyAndPolicy(){
+        return(
+            <div className='privacy-and-term'>
+                GDPR privacy policy requirements
+                Article 12 of the GDPR requires that you communicate information about your processing of personal data in a way that is:
+
+                concise
+                transparent
+                in clear and plain language
+                intelligible
+                easily accessible
+                free of charge
+                In general, most privacy laws require you to inform your users about the following:
+
+                Your name (or business name), location, and contact information
+                What information you’re collecting from them (including names, email addresses, IP addresses, and any other information)
+                What methods you are using to collect their information, e.g. cookies
+                The purpose for collecting this information
+                How you’re keeping their information safe
+                Whether or not it’s optional for them to share that information, how they can opt-out, and the consequences of doing so
+                Any third-party services you’re using to collect, process, or store that information (such as an e-mail newsletter service, or advertising network)
+                Following a GDPR privacy policy templates like the above can be a help on the way, but using a GDPR privacy policy generator (as we link to below) can be dangerous. You must be mindful of getting all of the relevant and required information about your website into your GDPR privacy policy.
+
+
+                <div className="col-sm-4 white-space">
+                    <button type="submit" className="btn btn-primary btn-login col-12">
+                        {this.state.isLoad ? <i className="loader fa fa-refresh fa-2x fa-spin " /> : "Send"}
+                    </button>
+                </div>
+            </div>
         )
     }
 
     render() {
         const { errorMessage, formValidation } = this.state
+        
         return (
             <div className="login">
                 <div className="container-fluid">
-                    {
-                        this.state.forgotPassword ? this.forgotPasswordForm(errorMessage, formValidation) : this.loginForm(errorMessage, formValidation)
-                    }
-                </div>
+                    <div className="card offset-lg-1">
+                            <div className="card-body">
+                                <img src={Logo} className="logo mb-5" alt="mlslogo" />
+                                    {
+                                    
+                                    this.state.forgotPassword ? this.forgotPasswordForm(errorMessage, formValidation) : this.loginForm(errorMessage, formValidation)
+                                    }
+                                <div className="links mt-3">
+                                    <span className='term-and-condition'>Privacy and Policy</span>
+                                    <span> &nbsp; | &nbsp; </span>
+                                    <span className='term-and-condition'>Terms and Conditions</span>
+                                </div>
+                            </div>
+                        </div>          
+                    <div className="copyright offset-lg-1 mt-5">
+                        <a target='blank' href='https://www.microlistics.com.au/'>© Microlistics {new Date().getFullYear()}</a>
+                    </div>
+                </div>                
             </div>
+            
         )
     }
 }
