4a282d715972c230cf0aa345264699dbee1785be
adhiyatna <adhiyatna@onebyone.io>
2020-06-22 15:59:02 +0700

    versioning

--- src/AppComponent/VersioningComponent.js
+++ src/AppComponent/VersioningComponent.js
@@ -29,7 +29,7 @@ class VersioningComponent extends Component {
     // }
 
     componentDidMount() {
-        // this.checkVersion();        
+        this.checkVersion();        
         
     }
 
