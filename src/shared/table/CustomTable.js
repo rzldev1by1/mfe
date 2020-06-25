197ef84691a24d3f762db10903ced08acf73ebcb
iyan cupliz <iyan.cupliz@gmail.com>
2020-06-26 00:50:17 +0700

    Table - update pagination

--- src/shared/table/CustomTable.js
+++ src/shared/table/CustomTable.js
@@ -1,120 +1,39 @@
+import _ from 'lodash'
 import React from 'react'
 import ReactTable from 'react-table-v6'
-import { Button, FormControl, Container, Row, Col, Modal } from 'react-bootstrap'
-import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
-import { MdSkipNext, MdSkipPrevious, MdClose } from 'react-icons/md'
+import { Button, Container, Row, Col, Modal } from 'react-bootstrap'
+import { MdClose } from 'react-icons/md'
 import { FaRegEdit, FaPencilAlt } from 'react-icons/fa'
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 import 'react-table-v6/react-table.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './CustomTable.css'
+
+// automatic column width
+const getColumnWidth = (rows, accessor, headerText) => {
+  const cellLength = Math.max(
+    ...rows.map(row => {
+      let value = '';
+      if (typeof accessor === 'string') {
+        value = _.get(row, accessor);
+      } else {
+        value = accessor(row);
+      }
+
+      if (typeof value === 'number') return value.toString().length;
+      return (value || '').length;
+    }),
+    headerText.length
+  );
+  return cellLength * 12
+}
 
 class CustomTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 0,
-      button: [1, 2, 3],
-      btnStat: "left",
-      goPage: '',
-      maxPage: null,
       showModal: false,
       editColumn: {},
       editColumnTemp: {}
-    }
-  }
-
-  handlePrev(page, maxPage) {
-    const { button, btnStat } = this.state
-    if (page !== 0) {
-      let newButton = button
-      let newBtnStat = btnStat
-      if (page < maxPage && page > 1) {
-        newButton[0] = newButton[0] - 1
-        newButton[1] = newButton[1] - 1
-        newButton[2] = newButton[2] - 1
-      }
-      if (page === 1) {
-        newBtnStat = 'left'
-      } else {
-        newBtnStat = 'mid'
-      }
-      this.setState({ page: page - 1, button: newButton, btnStat: newBtnStat })
-    }
-  }
-
-  handleNext(page, maxPage) {
-    const { button, btnStat } = this.state
-    if (page !== maxPage) {
-      let newButton = button
-      let newBtnStat = btnStat
-      if (page < (maxPage - 1) && page > 0) {
-        newButton[0] = newButton[0] + 1
-        newButton[1] = newButton[1] + 1
-        newButton[2] = newButton[2] + 1
-      }
-      if (page === (maxPage - 1)) {
-        newBtnStat = 'right'
-      } else {
-        newBtnStat = 'mid'
-      }
-      this.setState({ page: page + 1, button: newButton, btnStat: newBtnStat })
-    }
-  }
-
-  handlePrevEnd = () => {
-    const { button } = this.state
-    let newButton = button
-
-    newButton[0] = 1
-    newButton[1] = 2
-    newButton[2] = 3
-    this.setState({ page: 0, button: newButton, btnStat: 'left' })
-  }
-
-  handleNextEnd = (maxPage) => {
-    const { button } = this.state
-    let newButton = button
-
-    newButton[0] = maxPage - 2
-    newButton[1] = maxPage - 1
-    newButton[2] = maxPage
-    this.setState({ page: maxPage - 1, button: newButton, btnStat: 'right' })
-  }
-
-  handlePageNum(page, maxPage) {
-    const { button, btnStat } = this.state
-    let newButton = button
-    let newBtnStat = btnStat
-
-    if (page === 1) {
-      newBtnStat = 'left'
-      newButton[0] = 1
-      newButton[1] = 2
-      newButton[2] = 3
-    } else if (page === (maxPage + 1)) {
-      newBtnStat = 'right'
-      newButton[0] = maxPage - 1
-      newButton[1] = maxPage
-      newButton[2] = maxPage + 1
-    } else {
-      newBtnStat = 'mid'
-      newButton[0] = page - 1
-      newButton[1] = page
-      newButton[2] = page + 1
-    }
-    this.setState({ page: page - 1, button: newButton, btnStat: newBtnStat })
-  }
-
-  handleChangeGoPage = (maxPage, evt) => {
-    let value = evt.target.value
-    this.setState({ goPage: value, maxPage: maxPage })
-  }
-
-  handleKeyPress = (event) => {
-    const { goPage, maxPage } = this.state
-    if (event.key === 'Enter') {
-      this.handlePageNum(Number(goPage), maxPage)
     }
   }
 
@@ -159,77 +78,64 @@ class CustomTable extends React.Component {
     this.setState({ editColumnTemp: editColumn, showModal: false })
   }
 
-  headerIcon = (header, editColumn) => {
+  headerIcon = (data, header, editColumn) => {
     let listHeader = []
-    header && header.map((data, index) => {
-      if (editColumn[index] === undefined) {
+    header && header.map((h, index) => {
+      if (!editColumn[index]) {
         let withIcon = <span className="text-light-gray">
-          {data.Header}
-          {data.sortable ?
+          {h.Header}
+          {h.sortable ?
             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z"></path>
             </svg> : null
           }
         </span>
-
         let obj = {
-          "Header": withIcon,
-          "accessor": data.accessor,
-          'sortable': data.sortable || false,
-          'resizable': data.resizable || false,
-          // 'style': { textAlign: 'right' },
-          'Cell': data.Cell,
+          Header: withIcon,
+          Cell: h.Cell,
+          accessor: h.accessor,
+          sortable: h.sortable || false,
+          resizable: h.resizable || false,
+          width: getColumnWidth(data, h.accessor, h.Header),
+          // width: h.Header.length * 15,
         }
-
         return listHeader = [...listHeader, obj]
       } else {
         return listHeader = [...listHeader]
       }
     })
 
-    let editBtn = <div className="edit-column" onClick={this.showModal.bind(this, true)}><FaPencilAlt className="text-primary" /></div>
+    let editBtn = <div className="edit-column" onClick={this.showModal.bind(this, true)}><i className="iconU-edit text-primary" /></div>
     let obj = {
-      'Header': editBtn,
-      'accessor': 'editBtn',
-      'width': 50,
-      'style': { textAlign: 'center' }
+      Header: editBtn,
+      accessor: 'editBtn',
+      width: 50,
+      style: { textAlign: 'center' }
     }
     listHeader = [...listHeader, obj]
     return listHeader
   }
 
   render() {
-    const { // data, columns
-      page, button, btnStat, goPage, showModal, editColumn, editColumnTemp
-    } = this.state
-    let { title, data, fields, onClick, pageSize = 50, height } = this.props
-    const maxPage = Math.round(data.length / pageSize)
-    const length = Math.round(data.length)
-    const bottom = Math.round((page * pageSize) + 1)
-    const top = ((bottom + pageSize) - 1) > length ? length : (bottom + pageSize) - 1
-    const headerIcon = this.headerIcon(fields, editColumnTemp)
+    const { showModal, editColumn, editColumnTemp } = this.state
+    let { title, data, fields, onClick, height, pagination } = this.props
+    const headerIcon = this.headerIcon(data, fields, editColumnTemp)
     return (
       <React.Fragment>
         <ReactTable
           columns={headerIcon}
           data={data}
           showPagination={false}
-          page={page}
-          defaultPageSize={pageSize}
           style={{ height }}
-          minRows={10}
-          // getTheadThProps={(state, rowInfo, column, instance)=>{
-          //   console.log(instance)
-          //   return {
-          //     style: { cursor: !!onClick && 'pointer', textAlign: isNaN(rowInfo?.original[column.id]) ? 'left' : 'right' }
-          //   }
-          // }}
           getTdProps={(state, rowInfo, column, instance) => {
             return {
               onClick: (e, handleOriginal) => {
                 !!onClick && onClick(rowInfo.original, state, column, e, instance)
               },
-              style: { cursor: !!onClick && 'pointer', textAlign: isNaN(rowInfo?.original[column.id]) ? 'left' : 'right' }
+              style: {
+                cursor: !!onClick && 'pointer',
+                textAlign: isNaN(rowInfo?.original[column.id]) ? 'left' : 'right'
+              }
             }
           }}
           {...this.props}
@@ -283,82 +189,6 @@ class CustomTable extends React.Component {
             <Button variant="primary" className="px-5" onClick={this.saveEdit.bind(this, editColumn)} >Save</Button>
           </Modal.Footer>
         </Modal>
-
-
-
-        <Container fluid className="mt-4">
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3} className="p-0">
-              <div className="paging">
-                <Button
-                  variant="light" className="btn-paging p-0"
-                  onClick={this.handlePrevEnd.bind(this)}
-                >
-                  <MdSkipPrevious />
-                </Button>
-                <Button
-                  variant="light" className="btn-paging p-0"
-                  onClick={this.handlePrev.bind(this, page, maxPage)}
-                >
-                  <GrFormPrevious />
-                </Button>
-                <Button
-                  variant={btnStat === 'left' ? 'primary' : 'light'} className="btn-paging p-0"
-                  onClick={this.handlePageNum.bind(this, button[0], maxPage)}
-                >
-                  {button[0]}
-                </Button>
-                <Button
-                  variant={btnStat === 'mid' ? 'primary' : 'light'} className="btn-paging p-0"
-                  onClick={this.handlePageNum.bind(this, button[1], maxPage)}
-                >
-                  {button[1]}
-                </Button>
-                <Button
-                  variant={btnStat === 'right' ? 'primary' : 'light'} className="btn-paging p-0"
-                  onClick={this.handlePageNum.bind(this, button[2], maxPage)}
-                >
-                  {button[2]}
-                </Button>
-                <Button
-                  variant="light" className="btn-paging p-0"
-                  onClick={this.handleNext.bind(this, page, maxPage)}
-                >
-                  <GrFormNext />
-                </Button>
-                <Button
-                  variant="light" className="btn-paging p-0"
-                  onClick={this.handleNextEnd.bind(this, maxPage)}
-                >
-                  <MdSkipNext />
-                </Button>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3} className="col-go-page">
-              <div className="go-page d-flex">
-                <span className="p-2">Go to Page</span>
-                <FormControl
-                  placeholder=""
-                  className="form-go-page"
-                  value={goPage}
-                  onChange={this.handleChangeGoPage.bind(this, maxPage)}
-                  onKeyPress={this.handleKeyPress.bind(this)}
-                  type="number"
-                />
-                <Button variant="light" className="btn-go p-0 pl-3"> Go <GrFormNext /> </Button>
-              </div>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3} className="col-text-entries p-0">
-              <span> Showing </span>
-              <b> &nbsp; {`${bottom} to ${top} of ${length} `} </b>
-              &nbsp; entries
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3} className="btn-export">
-              {this.props.export}
-            </Col>
-          </Row>
-        </Container>
-
       </React.Fragment>
     )
   }
