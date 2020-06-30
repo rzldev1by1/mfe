719ad7b9e93f7ae5eca79476091269a47569e13e
brian capah <brian.capah@onebyone.io>
2020-06-30 08:48:43 +0700

    change baseurl

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 AppSetting = {
 	env: "DEVELOPMENT",
 	gatewayUrl: "http://apacheweb.onebyone.io:8080/request/httprequest.php",
-	endpointUrl: "http://apidev.microlistics.tech/",
+	endpointUrl: "https://apidev.microlistics.tech/",
 	// endpointUrl: "http://127.0.0.1:8000/",
 	// endpointUrl: "http://3.19.207.202/microlisticsapi/public/",
 	// "http://api.mls.onebyone.io:81/"
