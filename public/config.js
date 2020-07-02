8887509f337d7ab8087a77fd2b5c87111b9942e3
brian capah <brian.capah@onebyone.io>
2020-07-02 09:49:31 +0700

    redirect

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 AppSetting = {
 	env: "DEVELOPMENT",
 	gatewayUrl: "http://apacheweb.onebyone.io:8080/request/httprequest.php",
-	// endpointUrl: "https://apidev.microlistics.tech/",
-	endpointUrl: "http://127.0.0.1:8000/",
+	endpointUrl: "https://apidev.microlistics.tech/",
+	// endpointUrl: "http://127.0.0.1:8000/",
 	// endpointUrl: "http://3.19.207.202/microlisticsapi/public/",
 	// "http://api.mls.onebyone.io:81/"
 }
