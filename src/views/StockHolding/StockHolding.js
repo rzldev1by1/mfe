6cfd6f4600b58055a7f4e7c88c09f97d760dda66
brian capah <brian.capah@onebyone.io>
2020-07-02 11:05:28 +0700

    test

--- src/views/StockHolding/StockHolding.js
+++ src/views/StockHolding/StockHolding.js
@@ -500,6 +500,7 @@ class StockHolding extends Component {
 		return (
 			<React.Fragment>
 				<HeaderTitle title="Stock Holding" />
+				Test
 				<div className="animated fadeIn app-container">
 					<div className="card w-100 mb-3">
 						<form ref={this.searchForm} onSubmit={e => { e.preventDefault(); this.searchData() }}>
