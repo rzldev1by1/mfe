e0ae9d03e0bb38add5547606587d101f47de8b20
Fan Yansen <fan.yansen@onebyone.io>
2020-07-04 15:03:41 +0700

    Fix Space Between Text Search and Box Search

--- src/AppComponent/Search.js
+++ src/AppComponent/Search.js
@@ -9,7 +9,7 @@ const Search = (props) => {
                     <span className="input-group-text border-0 bg-transparent ml-2" style={{ padding: "0.4rem" }}>
                         <i className="iconU-search" />
                     </span>
-                    <input type="text" style={{ fontFamily: "Helvetica Neue Regular", backgroundColor: "transparent", marginTop: "0.4rem" }} className="form-control searchInput border-0" id="searchInput" name="searchInput" placeholder={props.placeholder} onChange={(e) => props.getValue(e.target.value)} />
+                    <input type="text" style={{ fontFamily: "Helvetica Neue Regular", backgroundColor: "transparent" }} className="form-control searchInput border-0" id="searchInput" name="searchInput" placeholder={props.placeholder} onChange={(e) => props.getValue(e.target.value)} />
                 </div>
             </div>
             <div className='search-column-right'>
