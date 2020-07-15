578e2063f3a3ebf4ed8a74825d83c8210d3c0b1b
adhiyatna <adhiyatna@onebyone.io>
2020-07-15 11:45:56 +0700

    update routes

--- src/routes.js
+++ src/routes.js
@@ -18,24 +18,18 @@ const StockMovement = React.lazy(() => import('./pages/StockMovement/StockMoveme
 const StockAgeProfile = React.lazy(() => import('./pages/StockAgeProfile/StockAgeProfile'));
 const UsersManagement = React.lazy(() => import('./pages/UserManagement/UserManagement'));
 const UsersManagementDetail = React.lazy(() => import('./pages/UserManagement/UserManagementDetail'));
-// const StockAgeProfile = React.lazy(() => import('./pages/StockAgeProfile/StockAgeProfile'));
+
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
 	{ path: '/', exact: true, name: 'Microlistics', component: Welcome },
 	{ path: '/stock-holding', exact: true, name: 'Stock Holding', component: StockHolding },
-	{ path: '/stock-holding:product/:client/:site', exact: true, name: 'Stock Holding Detail', component: StockHoldingDetail },
-	// { path: '/stockmovement', exact: true, name: 'Stock Movement', component: StockMovement },
-	{ path: '/stockageprofile', exact: true, name: 'Stock Age Profile', component: StockAgeProfile },
-
+	{ path: '/stock-holding:product/:client/:site', exact: true, name: 'Stock Holding Detail', component: StockHoldingDetail },	
 	{ path: '/purchase-order', exact: true, name: 'Purchase Order', component: PurchaseOrders },
-	{ path: '/purchase-order/:client/:orderdetail', exact: true, name: 'Purchase Order Detail', component: PurchaseOrdersDetail },
-	// { path: '/icons', exact: true, name: 'Sales Order', component: SalesOrder },
-
+	{ path: '/purchase-order/:client/:orderdetail', exact: true, name: 'Purchase Order Detail', component: PurchaseOrdersDetail },	
 	{ path: '/users-management', exact: true, name: 'User Management', component: UsersManagement },
 	{ path: '/stock-age-profile', exact: true, name: 'Stock Age Profile', component: StockAgeProfile },
-	{ path: '/users-management/:id/detail', exact: true, name: 'User Management', component: UsersManagementDetail },
-	// { path: '/users-management/create', exact: true, name: 'User Management', component: UsersManagementCreate },
+	{ path: '/users-management/:id/detail', exact: true, name: 'User Management', component: UsersManagementDetail },	
 	{ path: '/sales-orders/:client/:site/:orderno', exact: true, name: 'Sales Order Detail', component: SalesOrderDetail },
 	{ path: '/sales-orders', exact: true, name: 'Sales Orders', component: SalesOrder },
 	{ path: '/stock-movement', exact: true, name: 'Stock Movement', component: StockMovement },
