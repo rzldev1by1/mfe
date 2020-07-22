c164e19acb1874cd64383a32323f93fba57151a2
hotmasilaen <hotma.silaen@onebyone.io>
2020-07-22 14:36:49 +0700

    remove double quotes of stock holding page

--- src/pages/StockHolding/StockHoldingDetail.js
+++ src/pages/StockHolding/StockHoldingDetail.js
@@ -1,7 +1,7 @@
-import React from "react";
-import axios from "axios";
-import moment from "moment";
-import { CCard, CCardBody, CRow, CCol, CButton } from "@coreui/react";
+import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import { CCard, CCardBody, CRow, CCol, CButton } from '@coreui/react';
 import {
   Card,
   CardBody,
@@ -11,11 +11,11 @@ import {
   NavLink,
   TabPane,
   TabContent,
-} from "reactstrap";
-import CustomTable from "shared/table/CustomTable";
-import { tab1, tab1Inactive, tab2, tab2Inactive } from "./Helper";
-import HeaderTitle from "shared/container/TheHeader";
-import "./StockHolding.css";
+} from 'reactstrap';
+import CustomTable from 'shared/table/CustomTable';
+import { tab1, tab1Inactive, tab2, tab2Inactive } from './Helper';
+import HeaderTitle from 'shared/container/TheHeader';
+import './StockHolding.css';
 
 class SalesOrderDetail extends React.Component {
   // ref to get element height and calculate table height
@@ -23,46 +23,46 @@ class SalesOrderDetail extends React.Component {
   state = {
     dimension: { width: 0, height: 0 },
     stockDetail: [
-      { accessor: "batch", Header: "Batch", sortable: true, width: 130 },
-      { accessor: "rotadate", Header: "Rotadate", sortable: true, width: 100 },
-      { accessor: "ref3", Header: "Ref3", sortable: true, width: 100 },
-      { accessor: "ref4", Header: "Ref4", sortable: true, width: 100 },
-      { accessor: "qty", Header: "Qty", sortable: true, width: 110 },
-      { accessor: "weight", Header: "Weight", sortable: true, width: 115 },
-      { accessor: "pallet", Header: "Pallet", sortable: true, width: 120 },
-      { accessor: "price", Header: "Price", sortable: true, width: 120 },
-      { accessor: "pack_id", Header: "Pack Id", sortable: true, width: 180 },
+      { accessor: 'batch', Header: 'Batch', sortable: true, width: 130 },
+      { accessor: 'rotadate', Header: 'Rotadate', sortable: true, width: 100 },
+      { accessor: 'ref3', Header: 'Ref3', sortable: true, width: 100 },
+      { accessor: 'ref4', Header: 'Ref4', sortable: true, width: 100 },
+      { accessor: 'qty', Header: 'Qty', sortable: true, width: 110 },
+      { accessor: 'weight', Header: 'Weight', sortable: true, width: 115 },
+      { accessor: 'pallet', Header: 'Pallet', sortable: true, width: 120 },
+      { accessor: 'price', Header: 'Price', sortable: true, width: 120 },
+      { accessor: 'pack_id', Header: 'Pack Id', sortable: true, width: 180 },
     ],
     ForesCast: [
-      { accessor: "type", Header: "Type", sortable: true, width: 130 },
+      { accessor: 'type', Header: 'Type', sortable: true, width: 130 },
       {
-        accessor: "company",
-        Header: "Customer No",
+        accessor: 'company',
+        Header: 'Customer No',
         sortable: true,
         width: 140,
       },
-      { accessor: "orderno", Header: "Order No", sortable: true, width: 170 },
+      { accessor: 'orderno', Header: 'Order No', sortable: true, width: 170 },
       {
-        accessor: "effectivedate",
-        Header: "Order Date",
+        accessor: 'effectivedate',
+        Header: 'Order Date',
         sortable: true,
         width: 150,
       },
       {
-        accessor: "qtyexpected",
-        Header: "Expected In",
+        accessor: 'qtyexpected',
+        Header: 'Expected In',
         sortable: true,
         width: 150,
       },
       {
-        accessor: "qtycommitted",
-        Header: "Expected Out",
+        accessor: 'qtycommitted',
+        Header: 'Expected Out',
         sortable: true,
         width: 150,
       },
       {
-        accessor: "closingbalance",
-        Header: "Balance",
+        accessor: 'closingbalance',
+        Header: 'Balance',
         sortable: true,
         width: 140,
       },
@@ -70,11 +70,11 @@ class SalesOrderDetail extends React.Component {
     detail: {},
     products: [],
     datahead: [],
-    activeTab: "1",
+    activeTab: '1',
   };
   componentDidMount() {
     this.updateDimension();
-    window.addEventListener("resize", this.updateDimension);
+    window.addEventListener('resize', this.updateDimension);
     this.getDetail();
     this.getStockDetails();
     this.getForescast();
@@ -85,7 +85,7 @@ class SalesOrderDetail extends React.Component {
     }
   };
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimension);
+    window.removeEventListener('resize', this.updateDimension);
   }
 
   updateDimension = () => {
@@ -112,7 +112,7 @@ class SalesOrderDetail extends React.Component {
     const { product, client, site } = this.props.match.params;
     const url = `/stockdetail/${product}?client=${client}&site=${site}`;
     const { data } = await axios.get(url);
-    // const capitalize = (str, lower = false) => (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
+    // const capitalize = (str, lower = false) => (lower ? str.toLowerCase() : str).replace(/(?:^|\s|[''([{])+\S/g, match => match.toUpperCase());
     if (data.data.length) {
       this.setState({ products: data.data });
     }
@@ -122,11 +122,11 @@ class SalesOrderDetail extends React.Component {
     const url = `/stockbal?client=${client}&product=${product}&site=${site}`;
     const { data } = await axios.get(url);
     if (data) {
-      this.setState({ products: data[0][0]["available orders"] });
+      this.setState({ products: data[0][0]['available orders'] });
     }
   };
   formatDate = (date) => {
-    return date ? moment(date).format("DD/MM/YYYY") : "-";
+    return date ? moment(date).format('DD/MM/YYYY') : '-';
   };
   render() {
     // const { match, history } = this.props
@@ -165,102 +165,102 @@ class SalesOrderDetail extends React.Component {
       ? this.state.datahead[0].rotadate_type
       : null;
     return (
-      <div className="stock-holding-detail">
+      <div className='stock-holding-detail'>
         <HeaderTitle
           breadcrumb={[
-            { to: "/stock-holding", label: "Stock Holding" },
-            { to: "", label: this.props.match.params.product, active: true },
+            { to: '/stock-holding', label: 'Stock Holding' },
+            { to: '', label: this.props.match.params.product, active: true },
           ]}
         />
 
-        <div ref={this.section1} className="card-group section-1 mb-3">
+        <div ref={this.section1} className='card-group section-1 mb-3'>
           <CCard>
-            <CCardBody className="px-3 pt-2 pb-3 border-right">
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Site</CCol>
-                <CCol>{site || "-"}</CCol>{" "}
+            <CCardBody className='px-3 pt-2 pb-3 border-right'>
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Site</CCol>
+                <CCol>{site || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Client</CCol>{" "}
-                <CCol>{client || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Client</CCol>{' '}
+                <CCol>{client || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Product</CCol>{" "}
-                <CCol>{product || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Product</CCol>{' '}
+                <CCol>{product || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Description</CCol>{" "}
-                <CCol>{description || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Description</CCol>{' '}
+                <CCol>{description || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">UOM</CCol>{" "}
-                <CCol>{uom || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>UOM</CCol>{' '}
+                <CCol>{uom || '-'}</CCol>{' '}
               </CRow>
             </CCardBody>
           </CCard>
           <CCard>
-            <CCardBody className="px-3 pt-2 pb-3">
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Stock On Hand</CCol>{" "}
-                <CCol>{stock_on_hand || "-"}</CCol>{" "}
+            <CCardBody className='px-3 pt-2 pb-3'>
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Stock On Hand</CCol>{' '}
+                <CCol>{stock_on_hand || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Available Qty</CCol>{" "}
-                <CCol>{available_qty || "-"}</CCol>
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Available Qty</CCol>{' '}
+                <CCol>{available_qty || '-'}</CCol>
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Expected in Qty</CCol>{" "}
-                <CCol>{expected_in_qty || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Expected in Qty</CCol>{' '}
+                <CCol>{expected_in_qty || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Expected Out Qty</CCol>{" "}
-                <CCol>{expected_out_qty || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Expected Out Qty</CCol>{' '}
+                <CCol>{expected_out_qty || '-'}</CCol>{' '}
               </CRow>
-              <CRow className="mx-0">
-                <CCol className="text-light-gray px-0">Rotadate Type</CCol>{" "}
-                <CCol>{rotadate_type || "-"}</CCol>{" "}
+              <CRow className='mx-0'>
+                <CCol className='text-light-gray px-0'>Rotadate Type</CCol>{' '}
+                <CCol>{rotadate_type || '-'}</CCol>{' '}
               </CRow>
             </CCardBody>
           </CCard>
         </div>
 
         <CRow
-          className="align-items-center mx-0"
-          style={{ width: "max-content" }}
+          className='align-items-center mx-0'
+          style={{ width: 'max-content' }}
         >
-          <div className="stockDetails col-12 col-lg-12 col-md-12 col-sm-12 pl-0 pr-0">
-            <Nav tabs className="mx-0">
-              <div className="input-group">
-                <NavItem className="p-0">
+          <div className='stockDetails col-12 col-lg-12 col-md-12 col-sm-12 pl-0 pr-0'>
+            <Nav tabs className='mx-0'>
+              <div className='input-group'>
+                <NavItem className='p-0'>
                   <NavLink
                     className={
-                      "p-3 nav-link-cust" +
-                      (activeTab === "1" ? " tab-custom" : "")
+                      'p-3 nav-link-cust' +
+                      (activeTab === '1' ? ' tab-custom' : '')
                     }
-                    active={this.state.activeTab === "1"}
-                    onClick={() => this.activeTabIndex("1")}
+                    active={this.state.activeTab === '1'}
+                    onClick={() => this.activeTabIndex('1')}
                   >
-                    <div className="row rowTabCustom align-items-center tabColumn mx-0">
-                      <span className="tabTitleText">
-                        {activeTab === "1" ? tab1() : tab1Inactive()} Stock
+                    <div className='row rowTabCustom align-items-center tabColumn mx-0'>
+                      <span className='tabTitleText'>
+                        {activeTab === '1' ? tab1() : tab1Inactive()} Stock
                         Details
                       </span>
                     </div>
                   </NavLink>
                 </NavItem>
 
-                <NavItem className={"p-0"}>
+                <NavItem className={'p-0'}>
                   <NavLink
                     className={
-                      "p-3 nav-link-cust" +
-                      (activeTab === "2" ? " tab-custom" : "")
+                      'p-3 nav-link-cust' +
+                      (activeTab === '2' ? ' tab-custom' : '')
                     }
-                    active={this.state.activeTab === "2"}
-                    onClick={() => this.activeTabIndex("2")}
+                    active={this.state.activeTab === '2'}
+                    onClick={() => this.activeTabIndex('2')}
                   >
-                    <div className="row rowTabCustom align-items-center tabColumn mx-0">
-                      <span className="tabTitleText">
-                        {activeTab === "2" ? tab2() : tab2Inactive()} Stock
+                    <div className='row rowTabCustom align-items-center tabColumn mx-0'>
+                      <span className='tabTitleText'>
+                        {activeTab === '2' ? tab2() : tab2Inactive()} Stock
                         Balance Forecast
                       </span>
                     </div>
@@ -271,20 +271,20 @@ class SalesOrderDetail extends React.Component {
           </div>
         </CRow>
 
-        <Row className="mx-0">
-          <div className="col-12 col-lg-12 col-md-12 col-sm-12 mt-0 px-0 ">
-            <TabContent className="border-0" activeTab={this.state.activeTab}>
-              <TabPane className="p-0 stockDetails" tabId="1">
+        <Row className='mx-0'>
+          <div className='col-12 col-lg-12 col-md-12 col-sm-12 mt-0 px-0 '>
+            <TabContent className='border-0' activeTab={this.state.activeTab}>
+              <TabPane className='p-0 stockDetails' tabId='1'>
                 <CustomTable
-                  title="Stock Detail"
+                  title='Stock Detail'
                   height={this.state.dimension.height}
                   fields={stockDetail}
                   data={products}
                 />
               </TabPane>
-              <TabPane className="stockDetails" tabId="2">
+              <TabPane className='stockDetails' tabId='2'>
                 <CustomTable
-                  title="Stock ForesCast"
+                  title='Stock ForesCast'
                   height={this.state.dimension.height}
                   fields={ForesCast}
                   data={products}
