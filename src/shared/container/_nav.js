e10ecc53b00d9be80c028b6d799edd3ccd34520a
hotmasilaen <hotma.silaen@onebyone.io>
2020-07-22 13:41:55 +0700

    update detail stock holding page

--- src/shared/container/_nav.js
+++ src/shared/container/_nav.js
@@ -1,44 +1,44 @@
 export default [
   {
-    _tag: 'CSidebarNavItem',
-    name: 'STOCK HOLDING',
-    to: '/stock-holding',
-    icon: 'nav-sh',
-    key: "menu_inventory_stkHolding"
+    _tag: "CSidebarNavItem",
+    name: "STOCK HOLDING",
+    to: "/stock-holding",
+    icon: "nav-sh",
+    key: "menu_inventory_stkHolding",
   },
   {
-    _tag: 'CSidebarNavItem',
-    name: 'STOCK MOVEMENT',
-    to: '/stock-movement',
-    icon: 'nav-sm',
-    key: "menu_inventory_stkMovement"
+    _tag: "CSidebarNavItem",
+    name: "STOCK MOVEMENT",
+    to: "/stock-movement",
+    icon: "nav-sm",
+    key: "menu_inventory_stkMovement",
   },
   {
-    _tag: 'CSidebarNavItem',
-    name: 'STOCK AGE PROFILE',
-    to: '/stock-age-profile',
-    icon: 'nav-sap',
-    key: "menu_inventory_stkAgeProfile"
+    _tag: "CSidebarNavItem",
+    name: "STOCK AGE PROFILE",
+    to: "/stock-age-profile",
+    icon: "nav-sap",
+    key: "menu_inventory_stkAgeProfile",
   },
   {
-    _tag: 'CSidebarNavItem',
-    name: 'PURCHASE ORDERS',
-    to: '/purchase-order',
-    icon: 'nav-po',
-    key: "menu_orders_po_open"
+    _tag: "CSidebarNavItem",
+    name: "PURCHASE ORDERS",
+    to: "/purchase-order",
+    icon: "nav-po",
+    key: "menu_orders_po_open",
   },
   {
-    _tag: 'CSidebarNavItem',
-    name: 'SALES ORDERS',
-    to: '/sales-orders',
-    icon: 'nav-so',
-    key: "menu_orders_highSoOrder"
+    _tag: "CSidebarNavItem",
+    name: "SALES ORDERS",
+    to: "/sales-orders",
+    icon: "nav-so",
+    key: "menu_orders_highSoOrder",
   },
   {
-    _tag: 'CSidebarNavItem',
-    name: 'USER MANAGEMENT',
-    to: '/users-management',
-    icon: 'nav-um',
-    key: "usermanagement"
+    _tag: "CSidebarNavItem",
+    name: "USER MANAGEMENT",
+    to: "/users-management",
+    icon: "nav-um",
+    key: "usermanagement",
   },
-]
+];
