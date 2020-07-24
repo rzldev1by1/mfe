89bf4bfe9dcac79991ce17d6b642445400605feb
adhiyatna <adhiyatna@onebyone.io>
2020-07-24 10:18:06 +0700

    update UM

--- src/pages/UserManagement/tabs/NewUser.js
+++ src/pages/UserManagement/tabs/NewUser.js
@@ -30,8 +30,10 @@ class NewUser extends React.PureComponent {
            
         const {users} = this.props;
         let validation = { ...this.state.validation };
-         
-        validation.email["isValid"] = !textmail.match(regexMail)?false:true;           
+         console.log(users)
+         let isValidUser = users.filter((item)=>{return item.email === textmail}).length > 0?false:true;
+         let validFormat = !textmail.match(regexMail)?false:true;
+        validation.email["isValid"] = (isValidUser && validFormat)?true:false;           
         return validation;
       }
     checkNameValidation = (textName) => {     
@@ -116,7 +118,7 @@ class NewUser extends React.PureComponent {
                     <Col sm="4">
                         <input type="email" name="email" placeholder="Enter an email address" className={`form-control ${validation.email["isValid"]? '':validation.email["invalidClass"]}`} onChange={(e) => { this.onEmailChange(e); }} value={user.email || ''} />
                         <FormFeedback className="invalid-error-padding">
-                            wrong format email
+                            invalid email
                         </FormFeedback>
                     </Col>
                     <Col sm="4">
