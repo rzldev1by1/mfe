6466bd103ab70950c47e9a39bc2c261e55f16727
Fan Yansen <fan.yansen@onebyone.io>
2020-07-30 16:16:26 +0700

    Make Select Product show Option lists if char more than 3

--- src/pages/SalesOrder/tabs/DetailsTab.js
+++ src/pages/SalesOrder/tabs/DetailsTab.js
@@ -514,6 +514,8 @@ class CreateTab extends React.Component {
                 <td className="px-1 text-left">
                   <Select value={o.productVal || ''}
                     options={productData}
+                    menuIsOpen={o.productVal.length >= 3 ? true : false}
+                    onInputChange={(val) => this.lineSelectChange(i, 'productVal', val)}
                     onMenuOpen={() => {productStatus[i] = true; this.setState({ productStatus: productStatus })}}
                     onMenuClose={() => {productStatus[i] = false; this.setState({ productStatus: productStatus })}}
                     onChange={(val) => this.lineSelectChange(i, 'productVal', val)}
