a2f392ff124f55e95dc13ba3ccfcdc3b5621e596
briancapah <brian.capah@onebyone.io>
2020-08-03 20:48:01 +0700

    update client site name reg user stock holding

--- src/pages/StockHolding/StockHolding.js
+++ src/pages/StockHolding/StockHolding.js
@@ -176,6 +176,7 @@ class StockHolding extends React.PureComponent {
     const siteData = data.map(d => ({ value: d.site, label: `${d.site}: ${d.name}` }))
     const site = { value: 'all', label: 'All Site' }
     siteData.splice(0, 0, site)
+    this.props.dispatch({ type: 'SITE', data: siteData })
     this.setState({ siteData })
   }
   getClient = async () => {
@@ -183,6 +184,7 @@ class StockHolding extends React.PureComponent {
     const clientData = data.map(d => ({ value: d.code, label: `${d.code}: ${d.name}` }))
     const client = { value: 'all', label: 'All Client' }
     clientData.splice(0, 0, client)
+    this.props.dispatch({ type: 'CLIENT', data: clientData })
     this.setState({ clientData })
   }
   getStatus = async () => {
