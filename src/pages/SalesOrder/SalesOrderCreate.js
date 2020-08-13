294f60bea6188ebdfcf9043ad6757062e9235b5a
Dimas Syaiful <>
2020-08-14 05:19:49 +0700

    push notif

--- src/pages/SalesOrder/SalesOrderCreate.js
+++ src/pages/SalesOrder/SalesOrderCreate.js
@@ -3,10 +3,13 @@ import { Modal } from 'react-bootstrap'
 import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
 import DetailsTab from './tabs/DetailsTab.js'
 import ReviewTab from './tabs/ReviewTab'
+import MessageTab from '../../shared/MessageTab'
 
 class SalesOrderCreate extends React.PureComponent {
   state = {
     key: 'detail',
+    submit: false,
+    status: 'Failed to create order',
     data: { header: {}, lineDetail: [] },
     // data: { "header": { "site": { "value": "A", "label": "A : Australis A" }, "client": { "value": "AESOP", "label": "AESOP : Aesop" }, "orderType": { "value": "MVKT", "label": "MVKT: Move Orders" }, "orderId": "AB29123", "shipToAddress1": "Ark Street 12", "postCode": "291923", "state": "Victoria", "deliveryDate": "2020-07-02" }, "lineDetail": [{ "product": "product 1001", "productVal": { "value": "1001", "label": "1001", "i": 0 }, "qty": "2", "uom": { "value": "CARTON", "label": "CARTON" }, "disposition": "G", "dispositionVal": { "value": "G", "label": "G", "i": 9 } }] }
   }
@@ -19,8 +22,20 @@ class SalesOrderCreate extends React.PureComponent {
   }
   setData = (data) => {
     if (data.header && data.lineDetail) {
-      this.setState({ data, key: 'review' }, () => { 
+      this.setState({ data, key: 'review' }, () => {
       })
+    }
+  }
+  submitStatus = (status) => {
+    console.log(status)
+    this.setState({ submit: true })
+    if (status == 'create successfully') {
+      this.setState({ submit: true, status: "Successfully added" })
+      setTimeout(() => { this.onHide(); }, 3000)
+    }
+    else {
+      this.setState({ status });
+      this.setState({ submit: false });
     }
   }
   onHide = () => {
@@ -42,10 +57,10 @@ class SalesOrderCreate extends React.PureComponent {
         </Row>
         <Nav tabs className="px-7 mx-0">
           <NavItem className="mr-2"><NavLink className={`d-flex align-items-center ${key === 'detail' ? 'active' : null}`} onClick={() => this.onSelectTab('detail')}>
-          <span className='number-number-1'/>Order & Product Details
+            <span className='number-number-1' />Order & Product Details
           </NavLink></NavItem>
           <NavItem><NavLink className={`d-flex align-items-center ${key === 'review' ? 'active' : null}`} onClick={() => this.onSelectTab('review')}>
-          <span className='number-number-2'/> Review
+            <span className='number-number-2' /> Review
           </NavLink></NavItem>
         </Nav>
         <TabContent activeTab={this.state.key}>
@@ -56,11 +71,23 @@ class SalesOrderCreate extends React.PureComponent {
               {...this.props} />
           </TabPane>
           <TabPane tabId="review">
-            <ReviewTab
-              data={data}
-              back={() => this.onSelectTab('detail')}
-              submit={this.setData}
-              hide={this.onHide} />
+            {
+              this.state.submit ?
+                < MessageTab
+                  module={'Sales Order'}
+                  orderNo={data?.orderNo}
+                  status={this.state.status}
+                  back={() => this.onSelectTab('detail')}
+                  exit={() => this.onHide()} />
+                :
+                <ReviewTab
+                  data={data}
+                  back={() => this.onSelectTab('detail')}
+                  submit={this.setData}
+                  submitStatus={this.submitStatus}
+                  hide={this.onHide} />
+            }
+
           </TabPane>
         </TabContent>
       </Modal.Body>
