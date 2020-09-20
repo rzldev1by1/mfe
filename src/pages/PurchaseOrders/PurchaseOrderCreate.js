aab7dcb9b0f059a5d32f5ff9860102d286617b71
hotmasilaen <hotma.silaen@onebyone.io>
2020-09-20 23:50:24 +0700

    firefox css issue

--- src/pages/PurchaseOrders/PurchaseOrderCreate.js
+++ src/pages/PurchaseOrders/PurchaseOrderCreate.js
@@ -56,7 +56,7 @@ class SalesOrderCreate extends React.PureComponent {
           </Col>
         </Row>
         <Nav tabs className="px-7 m-0">
-          <NavItem className="mr-2"><NavLink style={{ height: "3.25rem" }} className={`d-flex align-items-center ${key === 'detail' ? 'active' : null}`} onClick={() => this.onSelectTab('detail')}>
+          <NavItem className="mr-2"><NavLink className={`d-flex height-nav align-items-center ${key === 'detail' ? 'active' : null}`} onClick={() => this.onSelectTab('detail')}>
             <span className='number-number-1' />Order & Product Details
           </NavLink></NavItem>
           <NavItem><NavLink className={`d-flex align-items-center ${key === 'review' ? 'active' : null}`} onClick={() => this.onSelectTab('review')}>
