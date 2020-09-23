3f9e12a7006d1280c4fbde8ec3a6ee231288dda5
Fan Yansen <fan.yansen@onebyone.io>
2020-09-23 14:20:52 +0700

    Refactor Style on Create User Management

--- src/pages/UserManagement/SiteReview.js
+++ src/pages/UserManagement/SiteReview.js
@@ -8,10 +8,18 @@ const siteReview = ({ sites }) => {
     const enable = 'Enabled';
     const disable = 'Disabled';
     return (<div>
-        <div className="flex-column mr-2">
-            <div className="row">
+        <div className="flex-column line-border mr-2 my-3">
+            {/* <div className="row">
                 <label className="col-6 text-muted" htmlFor="Site">Site
                 </label>
+            </div> */}
+            <div className={`d-flex `}>
+                <label className="col-6 text-muted-soft px-0 py-2" htmlFor="Site">Site
+                </label>
+
+                <div className="col-6 sub-header-review">
+                  {/* <button type="button" className={`btn px-1 float-right mb-2 ${isEnableAllSite?'btn-outline-All-notActive':'btn-outline-All-active'} `} onClick={onSiteEnableAll}>{`${isEnableAllSite?disableAll.toUpperCase():enableAll.toUpperCase()}`}</button> */}
+                </div>
             </div>
         </div>
 
@@ -19,12 +27,23 @@ const siteReview = ({ sites }) => {
         {
            (sites && sites.length)? sites.map((item, index) => {
                 return (<div className="flex-column mb-1 mr-2" key={index}>
-                    <div className="row height-40" key={index}>
+                    {/* <div className="row height-40" key={index}>
                         <label className="col-6 text-muted py-2" key={item.site}>{`${item.site} : ${item.name}`}</label>
 
                         <div className="col-6">
                             <button type="button" className={"px-1 no-outline " + ((item.status) ? "enable-active" : "enable-notactive")}>{`${item.status?enable.toUpperCase():disable.toUpperCase()}`}</button>
                         </div>
+                    </div> */}
+                    <div className="d-flex" key={index}>
+                        <label className="col-6 text-muted px-0 py-2" key={item.site}>{`${item.site}: ${item.name}`}</label>
+
+
+                        <div className="col-6">
+                            <button type="button" htmlFor={item.site}  className={"btn px-1 float-right btn-review "+("btn-outline-notActive")}>
+                            {`${item.status?enable.toUpperCase():disable.toUpperCase()}`}
+                            </button>
+                        </div>
+
                     </div>
                 </div>)
             }):null
