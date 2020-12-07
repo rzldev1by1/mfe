46e59bd63bb8e7bdd3f51b17e597c0f350400647
hotmasilaen <hotma.silaen@onebyone.io>
2020-12-07 10:49:06 +0700

    configuration Eslint and Prettier

--- src/Component/Breadcrumb/index.jsx
+++ src/Component/Breadcrumb/index.jsx
@@ -19,20 +19,20 @@ const TheHeader = (props) => {
     }
 
     return (
-        <CHeader withSubheader className="no-border no-shadow" id="stockMovement">
-            <CSubheader className="bg-transparent no-border no-shadow my-2">
-                <CToggler inHeader className="p-0 d-lg-none" onClick={toggleSidebarMobile} />
-                <CBreadcrumb className="no-border no-shadow mr-auto m-0 py-3">
-                    {props.breadcrumb ? props.breadcrumb.map((b, i) => {
+      <CHeader withSubheader className="no-border no-shadow" id="stockMovement">
+        <CSubheader className="bg-transparent no-border no-shadow my-2">
+          <CToggler inHeader className="p-0 d-lg-none" onClick={toggleSidebarMobile} />
+          <CBreadcrumb className="no-border no-shadow mr-auto m-0 py-3">
+            {props.breadcrumb ? props.breadcrumb.map((b, i) => {
                         return b.active ? <CBreadcrumbItem key={i} active>{b.label}</CBreadcrumbItem>
                             : <CBreadcrumbItem key={i}><Link to={b.to}>{b.label}</Link></CBreadcrumbItem>
                     }) : ''}
-                </CBreadcrumb>
-                <div className="c-subheader-nav">
-                    {props.button}
-                </div >
-            </CSubheader>
-        </CHeader>
+          </CBreadcrumb>
+          <div className="c-subheader-nav">
+            {props.button}
+          </div>
+        </CSubheader>
+      </CHeader>
     )
 }
 
--- src/Component/Modal/PopUpPages/service.js
+++ src/Component/Modal/PopUpPages/service.js
@@ -1,10 +1,10 @@
 export const closeModal = ({page, setPage}) => {
-    let newPage = {...page}
+    const newPage = {...page}
     newPage.notifPaging = false
     setPage(newPage)
 }
 export const showModal = ({page, setPage}) => {
-    let newPage = {...page}
+    const newPage = {...page}
     newPage.notifPaging = true
     setPage(newPage)
 }
