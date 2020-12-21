495c5ed01ce6f5365e3ab8ba0443ddc28f347f66
hotmasilaen <hotma.silaen@onebyone.io>
2020-12-21 10:36:34 +0700

    merge from R-Q3-007

--- src/Component/Search/index.jsx
+++ src/Component/Search/index.jsx
@@ -1,20 +1,12 @@
-/* eslint-disable max-len */
 import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types';
-import {useSelector, useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { CCard, CCardBody, CRow, CCol } from '@coreui/react'
 import Dropdown from '../Dropdown'
-import {getSite, getClient, getStatus, getOrderType, getTask} from '../../apiService/dropdown'
-
+import {getSite, getClient, getStatus, getOrderType, getTask} from '../../apiServices/dropdown'
 
 const Search = ({
     placeholder = '',
     searchHandler = null, // function when search button clicked
-    filterSite,
-    filterClient,
-    filterStatus,
-    filterOrderType,
-    filterTask
 }) => {
     // params
     const dispatch = useDispatch()
@@ -28,7 +20,7 @@ const Search = ({
     const {company, client, site} = user
 
     const search = (e) => {
-        if (e.key === 'Enter') searchHandler(desc)
+      if (e.key === 'Enter') searchHandler(desc)
     }
 
     useEffect(async () =>{
@@ -40,8 +32,9 @@ const Search = ({
       await getOrderType({dispatch, company, client})
       await getTask({dispatch, client, site})
     },[])
-
+    
     return (
+
       <CCard className="mb-3">
         <CCardBody className="p-3">
           <CRow className="mx-0">
@@ -64,49 +57,40 @@ const Search = ({
             </CCol>
             <CCol lg={9} className="px-0">
               <CRow className="mx-0">
-                <CCol sm={4} lg={2}>
+                <CCol sm={4} lg={2} className='px-0'>
                   <Dropdown
-                    className={`px-0 ${filterSite === true ? null : " d-none"}`}
+                    options={siteData}
+                    showTitle={false}
                     show
                     placeholder='Site'
-                    options={siteData}
-                    onChange={(val) => dispatch({site: val})}
                   />
                 </CCol>
-                <CCol sm={4} lg={2}>
+                <CCol sm={4} lg={2} className='px-3'>
                   <Dropdown
-                    className={`px-3 ${filterClient === true ? null : " d-none"}`}
+                    options={clientData}
                     show
                     placeholder='Client'
-                    options={clientData}
-                    onChange={(val) => dispatch({client: val})}
                   />
                 </CCol>
-                <CCol sm={4} lg={2}>
+                <CCol sm={4} lg={2} className='px-0'>
                   <Dropdown
-                    className={`px-0 ${filterStatus === true ? null : " d-none"}`}
+                    options={statusData}
                     show
                     placeholder='Status'
-                    options={statusData}
-                    // onChange={(val) => this.setClient(val)}
                   />
                 </CCol>
-                <CCol sm={4} lg={2}>
+                <CCol sm={4} lg={2} className='px-3'>
                   <Dropdown
-                    className={`px-0 ${filterOrderType === true ? null : " d-none"}`}
+                    options={orderTypeData}
                     show
                     placeholder='Order Type'
-                    options={orderTypeData}
-                    // onChange={(val) => this.setClient(val)}
                   />
                 </CCol>
-                <CCol sm={4} lg={2}>
+                <CCol sm={4} lg={2} className='px-0'>
                   <Dropdown
-                    className={`px-0 ${filterTask === true ? null : " d-none"}`} 
+                    options={taskData}
                     show
                     placeholder='Task'
-                    options={taskData}
-                    // onChange={(val) => this.setClient(val)}
                   />
                 </CCol>
                 <CCol sm={4} lg={2} className="px-0">
@@ -124,12 +108,6 @@ const Search = ({
         </CCardBody>
       </CCard>
     )
-}
-
-Search.propTypes = {
-  placeholder: PropTypes.string.isRequired,
-  filterSite: PropTypes.bool.isRequired,
-  filterClient : PropTypes.bool.isRequired
 }
 
 export default Search
