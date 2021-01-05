6b77c2534b504616a7836b9485638193bef6b1b7
Dimas Syaiful <dimas.syaiful@onebyone.com.sg>
2021-01-05 12:15:11 +0700

    [R-Q3-009] add change

--- src/Component/TableMaster/index.jsx
+++ src/Component/TableMaster/index.jsx
@@ -1,54 +1,53 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { CRow, CCol } from "@coreui/react"
-import Pagination from '../Pagination'
-import Table from '../Table'
+import React from 'react';
+import { CRow, CCol } from '@coreui/react';
+import Pagination from '../Pagination';
+import Table from '../Table';
 
 const TableMaster = ({
-    schemaColumn,
-    onClick,
-    data,
-    style,
-    module,
-    page,
-    setPage,
-    noDataText,
-    tableStatus,
-    pagination,
-    goto,
-    onClick,
-    exportData,
+  schemaColumn,
+  onClick,
+  data,
+  style,
+  module,
+  page,
+  setPage,
+  noDataText,
+  tableStatus,
+  pagination,
+  goto,
+  exportData,
 }) => {
-    return(
-      <div>
-        <Table
-          onClick={onClick}
-          schemaColumn={schemaColumn}
-          data={data}
-          style={style}
-          module={module}
-          onClick={onClick}
-          page={page}
-          setPage={setPage}
-          noDataText={noDataText}
-          tableStatus={tableStatus}
-        />
-        <CRow lg="12" className="mt-3 pagination-custom">
-          <CCol lg="7" className="px-0 margin-mr">
-            <Pagination
-              pagination={pagination} 
-              data={data}
-              page={page}
-              setPage={setPage}
-              goto={goto}
-              schemaColumn={schemaColumn}
-            />
-          </CCol>
-          <CCol lg="5" className="px-0 export-ml" />
-        </CRow>
-      </div>
-    )
-}
+  return (
+    <div>
+      <Table
+        onClick={onClick}
+        schemaColumn={schemaColumn}
+        data={data}
+        style={style}
+        module={module}
+        onClick={onClick}
+        page={page}
+        setPage={setPage}
+        noDataText={noDataText}
+        tableStatus={tableStatus}
+      />
+      <CRow lg="12" className="mt-3 pagination-custom">
+        <CCol lg="7" className="px-0 margin-mr">
+          <Pagination
+            pagination={pagination}
+            data={data}
+            page={page}
+            setPage={setPage}
+            goto={goto}
+            schemaColumn={schemaColumn}
+          />
+        </CCol>
+        <CCol lg="5" className="px-0 export-ml" />
+      </CRow>
+    </div>
+  );
+};
 
 export default TableMaster;
