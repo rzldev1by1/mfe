51223f25cbfc9bb80cc511b7d76d6701b7497467
hotmasilaen <hotma.silaen@onebyone.io>
2021-01-05 10:07:18 +0700

    reset hard

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Component
 const Component = React.lazy(() => import('../src/pages/Component'));
 const PurchaseOrders = React.lazy(() => import('../src/pages/PurchaseOrders'));
-const PurchaseOrdersDetail = React.lazy(() => import('../src/pages/PurchaseOrders/Detail'));
+// const PurchaseOrdersDetail = React.lazy(() => import('../src/pages/PurchaseOrders/Detail'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -21,12 +21,12 @@ const routes = [
 		name: 'Purchase Order', 
 		component: PurchaseOrders
 	},
-	{ 
-		path: '/purchase-order/:site/:client/:orderdetail',
-		exact: true,
-		name: 'Purchase Order Detail', 
-		component: PurchaseOrdersDetail
-	},
+	// { 
+	// 	path: '/purchase-order/:site/:client/:orderdetail',
+	// 	exact: true,
+	// 	name: 'Purchase Order Detail', 
+	// 	component: PurchaseOrdersDetail
+	// },
 	// Component End -----------------------------------------------------------
 ];
 
