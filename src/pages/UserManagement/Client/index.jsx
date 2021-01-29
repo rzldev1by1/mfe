ce4a211f1066f110ef632fa63b3763bc71622679
Dimas Syaiful <dimas.syaiful@onebyone.com.sg>
2021-01-29 16:29:27 +0700

    [R-Q5-007] Update User Management

---
+++ src/pages/UserManagement/Client/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import '../index.scss';
+import { onClientStatusClick, onEnabledAllClient } from '../services';
+
+const client = ({ clients, isEnableAllClient, state, setState }) => {
+  const disableAll = 'Disable All';
+  const enableAll = 'Enable All';
+  const enable = 'Enabled';
+  const disable = 'Disabled';
+
+  return (
+    <div>
+      <div className={`flex-column line-border my-3 `}>
+        <div className={`d-flex `}>
+          <label className="text-muted-soft col-6 p-0 py-2" htmlFor="Client">
+            Client
+          </label>
+
+          <div className="col-6 client-enableall-padding">
+            <button
+              type="button"
+              className={`btn float-right px-1 mb-2 ${
+                isEnableAllClient ? 'btn-outline-All-notActive' : 'btn-outline-All-active'
+              } `}
+              onClick={() => onEnabledAllClient({ state, setState })}
+            >
+              {`${isEnableAllClient ? disableAll.toUpperCase() : enableAll.toUpperCase()}`}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="client-areas">
+        {clients && client.length
+          ? clients.map((item, index) => {
+              return (
+                <div className="flex-column" key={index}>
+                  <div className="d-flex mb-1" key={index}>
+                    <label className="text-muted col-6 section-value-text p-0 py-2 " key={item.code}>
+                      {`${item.code}: ${item.name}`}
+                    </label>
+                    <div className="col-6">
+                      <button
+                        type="button"
+                        htmlFor={item.code}
+                        className={
+                          'btn px-1 float-right ' + (item.status ? 'btn-outline-active' : 'btn-outline-notActive')
+                        }
+                        onClick={(e) => {
+                          onClientStatusClick({ e, index, state, setState });
+                        }}
+                      >
+                        {`${item.status ? enable.toUpperCase() : disable.toUpperCase()}`}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default client;
