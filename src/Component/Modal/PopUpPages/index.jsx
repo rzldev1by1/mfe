b2b1f44dc5bfd8fd398fb5e07aeef25bcf0396f0
Aldi <alditrifaldi@onebyone.com.sg>
2021-03-09 09:30:06 +0700

    Change Styling

--- src/Component/Modal/PopUpPages/index.jsx
+++ src/Component/Modal/PopUpPages/index.jsx
@@ -10,48 +10,48 @@ import "./index.scss";
 
 
 class PopUpPages extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
     }
+  }
 
-    render = () => {
-        const { page, setPage, xLastPage } = this.props
-        const newPage = { ...page }
-        return (
-          <Modal
-            isOpen={newPage.notifPaging}
-            centered            
-            onOpened={() => newPage.notifPaging ? setTimeout(() =>{ closeModal({ page, setPage }) }, 36000) : {}}
-            contentClassName="modal-content-paging box-er-pagination"
-            closeOnBackdrop={false}
+  render = () => {
+    const { page, setPage, xLastPage } = this.props
+    const newPage = { ...page }
+    return (
+      <Modal
+        isOpen={newPage.notifPaging}
+        centered
+        onOpened={() => newPage.notifPaging ? setTimeout(() => { closeModal({ page, setPage }) }, 36000) : {}}
+        contentClassName="modal-content-paging"
+        closeOnBackdrop={false}
+      >
+        <ModalBody>
+          <div
+            className="text-right px-0"
+            style={{ fontSize: '14px' }}
+            onClick={() => closeModal({ page, setPage })}
           >
-            <ModalBody>
-              <div 
-                className="text-right px-0" 
-                style={{ fontSize: '14px' }} 
-                onClick={() => closeModal({ page, setPage })}
-              >
-                <i className="iconU-close pointer" />
-              </div>
-              <div className="d-flex d-inline-flex">
-                <img src={logoConfirm} alt="logo" style={{ width: "20%", height: "20%" }} />
-                <div className="pl-3 font">
-                  Only
-                  {' '}  
-                  {xLastPage}
-                  {xLastPage === 1 ? " page is " : " pages are "}
+            <i className="iconU-close pointer" />
+          </div>
+          <div className="d-flex d-inline-flex">
+            <img src={logoConfirm} alt="logo" style={{ width: "20%", height: "20%" }} />
+            <div className="pl-3 font">
+              Only
                   {' '}
-                  available on this screen, please try again. 
+              {xLastPage}
+              {xLastPage === 1 ? " page is " : " pages are "}
+              {' '}
+                  available on this screen, please try again.
                   <br />
-                </div>
-              </div>
-            </ModalBody>
-          </Modal>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
 
-        );
-    }
+    );
+  }
 }
 
 export default PopUpPages;
