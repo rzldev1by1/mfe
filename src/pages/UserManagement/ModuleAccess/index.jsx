2a7bf5fcab636011f8e107559a5b96330633e8ee
hotmasilaen <hotma.silaen@onebyone.io>
2021-09-02 15:39:39 +0700

    remove supplier module from module access

--- src/pages/UserManagement/ModuleAccess/index.jsx
+++ src/pages/UserManagement/ModuleAccess/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../index.scss';
 import { onModuleAccessClick, onEnabledAllModuleAccess } from '../services';
 
+const isDevelopment = process.env.REACT_APP_DEV;
+
 const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isReadOnly , module}) => {
   const disableAll = 'Disable All';
   const enableAll = 'Enable All';
@@ -10,13 +12,13 @@ const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isRead
 
   const height = window.innerHeight - 680;
   const heightDetail = window.innerHeight - 510;
-
+console.log(moduleAccess);
   return (
     <div>
       <div className={`flex-column line-border mr-2 my-3`}>
         <div className={`d-flex `}>
           <label className="col-6 text-muted-soft px-0 py-2" htmlFor="moduleAccess">
-            Module Access
+            Module Accessdd
           </label>
           <div className="col-6 pr-4">
             <button
@@ -34,23 +36,23 @@ const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isRead
 
       <div className="client-areas pr-3" 
             style={ module === 'detail' ? { height:heightDetail, minHeight:heightDetail} : {height:height, minHeight:height}}>
-        {moduleAccess && moduleAccess.length
+        {moduleAccess && moduleAccess.length && moduleAccess.menu_id !== "menu_manageUsers_supplierUsers"
           ? moduleAccess.map((item, index) => {
+            if(isDevelopment === 'true' && item.menu_name !== "Manage Supplier Users"){
               return (
                 <div className="flex-column mb-1" key={index}>
                   <div className="d-flex" key={index}>
                     <label className="col-6 text-muted px-0 py-2" key={item.menu_id}>
-                      { item.menu_name.toLowerCase() === 'create sales order' ? 'Sales Orders' : 
-                        item.menu_name.toLowerCase() === 'manage supplier users' ? 'Supplier Management' : item.menu_name}
+                      { item.menu_name.toLowerCase() === 'create sales order' ? 'Sales Orders' : item.menu_name}
                     </label>
                     <div className="col-6">
                       <button
                         type="button"
                         htmlFor={item.menu_name}
                         className={
-                          'btn px-1 float-right ' +
-                          (!isReadOnly && item.status ? 'btn-outline-active' : 'btn-outline-notActive') +
-                          (isReadOnly ? ' btn-review' : '')
+                          `btn px-1 float-right ${ 
+                          !isReadOnly && item.status ? 'btn-outline-active' : 'btn-outline-notActive' 
+                          }${isReadOnly ? ' btn-review' : ''}`
                         }
                         onClick={(e) => {
                           if (!isReadOnly) onModuleAccessClick({ e, index, state, setState });
@@ -62,6 +64,8 @@ const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isRead
                   </div>
                 </div>
               );
+            }
+             
             })
           : null}
       </div>
