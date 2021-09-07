56251c854b59e17bbf263df79ba72b1238a15cc3
Hanjarraes <hanjar.muhammad@onebyone.com.sg>
2021-09-07 14:22:58 +0700

    Update DarkMode

--- src/pages/UserManagement/ModuleAccess/index.jsx
+++ src/pages/UserManagement/ModuleAccess/index.jsx
@@ -4,7 +4,7 @@ import { onModuleAccessClick, onEnabledAllModuleAccess } from '../services';
 
 const isDevelopment = process.env.REACT_APP_DEV;
 
-const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isReadOnly , module}) => {
+const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isReadOnly, module }) => {
   const disableAll = 'Disable All';
   const enableAll = 'Enable All';
   const enable = 'Enabled';
@@ -13,7 +13,7 @@ const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isRead
   const height = window.innerHeight - 700;
   const heightDetail = window.innerHeight - 530;
 
-  const filterModuleAccess = moduleAccess.filter((item) => { return isDevelopment === 'true' ? item : item.menu_id !== "menu_manageUsers_supplierUsers"})
+  const filterModuleAccess = moduleAccess.filter((item) => { return isDevelopment === 'true' ? item : item.menu_id !== "menu_manageUsers_supplierUsers" })
   return (
     <div>
       <div className={`flex-column line-border mr-2 my-3`}>
@@ -38,35 +38,26 @@ const moduleAccess = ({ moduleAccess, isEnableAllModule, state, setState, isRead
         style={module === 'detail' ? { height: heightDetail, minHeight: heightDetail } : { height: height, minHeight: height }}>
         {moduleAccess && moduleAccess.length
           ? filterModuleAccess.map((item, index) => {
-              return (
-                <div className="flex-column mb-1" key={index}>
-                  <div className="d-flex" key={index}>
-                    <label className="col-6 text-muted px-0 py-2" key={item.menu_id}>
-                      { item.menu_name.toLowerCase() === 'create sales order' ? 'Sales Orders' : 
-                        item.menu_name.toLowerCase() === 'manage supplier users' ? 'Supplier Management' : item.menu_name}
-                    </label>
-                    <div className="col-6">
-                      <button
-                        type="button"
-                        htmlFor={item.menu_name}
-                        className={
-                          'btn px-1 float-right ' +
-                          (!isReadOnly && item.status ? 'btn-outline-active' : 'btn-outline-notActive') +
-                          (isReadOnly ? ' btn-review' : '')
-                        }
-                        onClick={(e) => {
-                          if (!isReadOnly) onModuleAccessClick({ e, index, state, setState });
-                        }}
-                      >
-                        {`${item.status ? enable.toUpperCase() : disable.toUpperCase()}`}
-                      </button>
-                    </div>
+            return (
+              <div className="flex-column mb-1" key={index}>
+                <div className="d-flex" key={index}>
+                  <label className="col-6 text-muted px-0 py-2" key={item.menu_id}>
+                    {item.menu_name.toLowerCase() === 'create sales order' ? 'Sales Orders' :
+                      item.menu_name.toLowerCase() === 'manage supplier users' ? 'Supplier Management' : item.menu_name}
+                  </label>
+                  <div className="col-6">
+                    <button
+                      type="button"
+                      htmlFor={item.menu_name}
+                      className={'btn px-1 float-right ' + (!isReadOnly && item.status ? 'btn-outline-active' : 'btn-outline-notActive') + (isReadOnly ? ' btn-review' : '')}
+                      onClick={(e) => { if (!isReadOnly) onModuleAccessClick({ e, index, state, setState }); }}>
+                      {`${item.status ? enable.toUpperCase() : disable.toUpperCase()}`}
+                    </button>
                   </div>
                 </div>
               </div>
             );
-          })
-          : null}
+          }) : null}
       </div>
     </div>
   );
--- src/shared/container/TheHeader.js
+++ src/shared/container/TheHeader.js
@@ -39,42 +39,40 @@ const TheHeader = (props) => {
   return (
     <CHeader withSubheader className="no-border no-shadow">
       <div className="header-mls-text">
-        <Link to="/" className="text-logo text-white">Microlistics </Link>
-        <div class="v-header-mls"></div>
+        <Link to="/" className="text-logo text-white pt-2">Microlistics </Link>
       </div>
 
       <CSubheader className="bg-transparent acount-mls">
         <div className="mfe-2 c-subheader-nav">
-          <CDropdown>
+          <CDropdown className="d-flex align-items-center" style={{ height: "69px" }}>
             <CDropdownToggle className="no-shadow alig-items-center pr-0 d-flex align-items-center">
-              <div class="v-header-mls pr-3"></div>
-              <div className="d-flex">
+              <div className="d-flex" style={{ marginLeft: "14%" }}>
                 <span className="icon-group_4690 icon-profil-user" />
                 <div className="mx-3 text-left ">
-                  <p className="my-0 ">{user?.name}</p>
+                  <p className="my-0 " style={{ fontSize: "118%" }}>{user?.name}</p>
                   <p className="my-0 ">{`${user.email} . ${user?.name}`}</p>
                 </div>
               </div>
             </CDropdownToggle>
             <CDropdownMenu className="drop-menu-profil p-0">
-              <CDropdownItem className="menu-item-profil">
-                <div className="menu-item-profil d-flex">
+              <CDropdownItem className="menu-item-profil p-0">
+                <div className="menu-item-profil d-flex p-2">
                   <span className="icon-icon_feather_users icon-drop-menu" />
                   <div className="d-flex align-items-md-center" style={{ paddingRight: "6rem" }}>User Details
                     <div style={{ marginLeft: "100%" }}>TTL </div>
                   </div>
                 </div>
               </CDropdownItem>
-              <CDropdownItem className="menu-item-profil">
-                <div className="menu-item-profil d-flex">
+              <CDropdownItem className="menu-item-profil p-0">
+                <div className="menu-item-profil d-flex p-2">
                   <span className="icon-group_8 icon-drop-menu" />
                   <div className="d-flex pr-2 align-items-md-center">Company License
                     <div style={{ marginLeft: "45%" }} >Free</div>
                   </div>
                 </div>
               </CDropdownItem>
-              <CDropdownItem className="menu-item-profil">
-                <div className="menu-item-profil d-flex">
+              <CDropdownItem className="menu-item-profil p-0">
+                <div className="menu-item-profil d-flex p-2">
                   <span className="icon-bell icon-drop-menu" />
                   <div className="d-flex align-items-md-center" style={{ paddingRight: "17rem" }}>Notification
                     <div style={{ marginLeft: "104%" }}> 445</div>
@@ -82,28 +80,29 @@ const TheHeader = (props) => {
                 </div>
               </CDropdownItem>
               <div className="m-info">
-                <p className="my-0 py-2" style={{ paddingLeft: "6%" }}>
+                <p className="my-0 py-2" style={{ paddingLeft: "2%" }}>
                   <div className=" d-flex align-items-md-center">
                     <span className="icon-group_4799 icon-drop-menu" />DarkMode
                     <div style={{ marginLeft: "40%" }}>
                       {/* <button onClick={() => DarkModeChange({ changeMode, setChangeMode })}>tes</button> */}
-                      <CSwitch shape={'pill'} color={'secondary'} onClick={() => dispatch({ type: 'DARKMODE', data: !darkModes })} defaultChecked />
+                      <CSwitch shape={'pill'} color={'secondary'} variant="outline" defaultChecked className="switch-profil" onClick={() => dispatch({ type: 'DARKMODE', data: !darkModes })} />
                     </div>
                   </div>
                 </p>
               </div>
-              <CDropdownItem>
-                <div className="menu-item-profil d-flex">
+              <CDropdownItem className="menu-item-profil p-0">
+                <div className="menu-item-profil d-flex p-2">
                   <span className="icon-group_9 icon-drop-menu" />
-                  <div className="d-flex  align-items-md-center">Language
+                  <div className="d-flex pr-2 align-items-md-center">Language
                     <div style={{ marginLeft: "57%" }}> English US </div>
                   </div>
                 </div>
               </CDropdownItem>
-              <CDropdownItem onClick={() => signOut()}>
-                <div className="menu-item-profil d-flex">
+              <CDropdownItem onClick={() => signOut()} className="menu-item-profil p-0">
+                <div className="menu-item-profil d-flex p-2">
                   <span className="icon-log-out icon-drop-menu" />
-                  <div className="d-flex align-items-md-center" style={{ paddingRight: "21rem" }}>Logout</div>
+                  <div className="d-flex align-items-md-center" style={{ paddingRight: "17rem" }}>Logout
+                  </div>
                 </div>
               </CDropdownItem>
             </CDropdownMenu>
