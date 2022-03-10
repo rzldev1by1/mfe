5daa9e2125c96f73f2ca4c572f194f1bc445eccf
Hanjarraes <hanjar.muhammad@onebyone.com.sg>
2022-03-10 13:25:38 +0700

    Update Logic Pagination Stock Movement

---
+++ src/Component/Pagination/PagingMove.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { BsChevronLeft, BsChevronRight, BsChevronBarLeft } from 'react-icons/bs';
+import PopUpPages from 'Component/Modal/PopUpPages';
+import { numberCheck, onChange, goToPage, changePage } from 'Component/Pagination/service';
+import './Pagination.scss';
+
+const PagingMove = ({ startIndex, endIndex, total, activePage, setActivePage, show}) => {
+  const [page, setPage] = useState({
+    notifPaging: false,
+    goPage: 1,
+  });
+
+  const pages = Math.ceil(total / show);
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+        setPage(e);
+    }
+  };
+  const searchForm = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={searchForm}>
+        <div style={{ width: 'fit-content', height: '49px' }} className="d-flex">
+          <div className={`page-item border-right-none ${activePage === 1 ? 'text-muted-soft' : ' text-muted-dark click-tab'}`}
+             onClick={() => setActivePage(1)}
+          >
+            <BsChevronBarLeft className="icon-size-paging-double" />
+          </div>
+          <div className={`page-item paging-previous ${activePage === 1 ? 'text-muted-soft' : ' text-muted-dark click-tab'}`}
+             onClick={() => setActivePage(activePage === 1 ? 1 : activePage - 1)}
+          >
+            <BsChevronLeft className="icon-size-paging" />
+          </div>
+          <div className={`d-flex align-items-center bg-paging-search pl-2 pr-2`}>
+            <input
+              id="paging-number"
+              type="number"
+              placeholder={activePage}
+              className="number-pag rounded input-paging"
+              onChange={(e) => onChange({ e, page, setPage })}
+              onKeyDown={(e) => search(e)}
+              min="1"
+              max={pages > 0 ? pages : 1}
+              onKeyPress={(e) => numberCheck(e)}
+            />
+            <span className="text-muted-soft ml-2">of</span>
+            <span className="text-muted-soft ml-2">{isNaN(pages) ? 1 : pages}</span>
+          </div>
+          <div className={`page-item margin-none-left border-left-none ${activePage >= pages ? 'text-muted-soft' : ' text-muted-dark click-tab'}`}
+            onClick={() => setActivePage(pages > activePage ? activePage+1 : pages)}
+          >
+            <BsChevronRight className=" icon-size-paging" />
+          </div>
+            <span className={`text-muted-s px-3 d-flex alig align-items-center`}>
+              <b className="text-muted-soft mr-1" style={{ fontWeight: '400' }}>
+                {' '}
+                {`Showing`}
+              </b>
+              <b className="text-muted-dark mr-1">
+                {`${isNaN(startIndex) ? 0 : startIndex} to ${isNaN(endIndex) ? 0 : endIndex} of ${total === undefined ? 0 : total}`}
+              </b>
+              <b className="text-muted-soft" style={{ fontWeight: '400' }}>
+                {' '}
+                {`entries`}
+              </b>
+            </span>
+        </div>
+      </form>
+      <PopUpPages page={page} setPage={setPage} xLastPage={pages} />
+    </div>
+  );
+};
+
+export default PagingMove;
--- src/Component/Pagination/service.js
+++ src/Component/Pagination/service.js
@@ -2,7 +2,7 @@
 /* eslint-disable radix */
 import { getSummaryData, getForescast, getDetailData } from '../../apiService';
 
-export const onActivePageChange = ({ e, pagination, goto, dispatch, module, props , searchFilter, user}) => {
+export const onActivePageChange = ({ e, pagination, goto, dispatch, module, props, searchFilter, user }) => {
   const active = parseInt(e > 1 ? e : 1);
   let paramPaging = '';
   let paramType = '';
@@ -52,30 +52,32 @@ export const onActivePageChange = ({ e, pagination, goto, dispatch, module, prop
   } else {
     dispatch({ type: paramPaging || 'PAGING', data: { ...pagination, active } });
   }
-  let arraySummary = ['StockHolding', 'purchaseOrder' , 'salesOrder','UserManagement' ]
-  if (arraySummary.includes(module)  ) {
-    getSummaryData({ dispatch, 
-                    active: active, 
-                    module, 
-                    props, 
-                    siteVal: searchFilter.siteVal,
-                    clientVal: searchFilter.clientVal,
-                    orderType: searchFilter.orderType,
-                    task: searchFilter.task,
-                    status: searchFilter.status, 
-                    user})
+  let arraySummary = ['StockHolding', 'purchaseOrder', 'salesOrder', 'UserManagement']
+  if (arraySummary.includes(module)) {
+    getSummaryData({
+      dispatch,
+      active: active,
+      module,
+      props,
+      siteVal: searchFilter.siteVal,
+      clientVal: searchFilter.clientVal,
+      orderType: searchFilter.orderType,
+      task: searchFilter.task,
+      status: searchFilter.status,
+      user
+    })
   }
 
   if (module === 'StockHoldingForecast') {
-    getForescast({ dispatch, active: active, module , props});
+    getForescast({ dispatch, active: active, module, props });
   }
-  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail' , 'SalesOrdersDetail']
-  if (arrayDetail.includes(module)  ) {
-    getDetailData({ dispatch, active: active, module , props});
+  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail', 'SalesOrdersDetail']
+  if (arrayDetail.includes(module)) {
+    getDetailData({ dispatch, active: active, module, props });
   }
 };
 
-export const goToPage = ({ goto, pagination, page, setPage, dispatch, module, props,  searchFilter, user }) => {
+export const goToPage = ({ goto, pagination, page, setPage, dispatch, module, props, searchFilter, user }) => {
   const newPage = { ...page };
   let paramPaging = '';
   let paramType = '';
@@ -128,7 +130,7 @@ export const goToPage = ({ goto, pagination, page, setPage, dispatch, module, pr
     newPage.notifPaging = true;
     setPage(newPage);
     return 0;
-  }else{
+  } else {
     dispatch({ type: paramType, data: [] });
   }
 
@@ -137,34 +139,37 @@ export const goToPage = ({ goto, pagination, page, setPage, dispatch, module, pr
   } else {
     dispatch({ type: paramPaging || 'PAGING', data: { ...pagination, active: newPage.goPage } });
   }
-  let arraySummary = ['StockHolding', 'purchaseOrder' , 'salesOrder','UserManagement' ]
-  if (arraySummary.includes(module)  ) {
-    getSummaryData({ dispatch, 
-                   active: newPage.goPage, 
-                   module, 
-                   props,  
-                   siteVal: searchFilter.siteVal,
-                   clientVal: searchFilter.clientVal,
-                   orderType: searchFilter.orderType,
-                   task: searchFilter.task,
-                   status: searchFilter.status,
-                    user });
-    }
-  
-  if (module === 'SupplierManagement'){
-    getSummaryData({ dispatch, 
-      active: newPage.goPage, 
-      module, 
+  let arraySummary = ['StockHolding', 'purchaseOrder', 'salesOrder', 'UserManagement']
+  if (arraySummary.includes(module)) {
+    getSummaryData({
+      dispatch,
+      active: newPage.goPage,
+      module,
       props,
-      user  
-      });
+      siteVal: searchFilter.siteVal,
+      clientVal: searchFilter.clientVal,
+      orderType: searchFilter.orderType,
+      task: searchFilter.task,
+      status: searchFilter.status,
+      user
+    });
+  }
+
+  if (module === 'SupplierManagement') {
+    getSummaryData({
+      dispatch,
+      active: newPage.goPage,
+      module,
+      props,
+      user
+    });
   }
 
   if (module === 'StockHoldingForecast') {
     getForescast({ dispatch, active: newPage.goPage, module, props });
   }
-  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail' , 'SalesOrdersDetail']
-  if (arrayDetail.includes(module)  ) {
+  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail', 'SalesOrdersDetail']
+  if (arrayDetail.includes(module)) {
     getDetailData({ dispatch, active: newPage.goPage, module, props });
   }
 };
@@ -186,7 +191,7 @@ export const numberCheck = (e) => {
   }
 };
 
-export const changePage = ({active, dispatch, module, props, searchFilter, user}) =>{
+export const changePage = ({ active, dispatch, module, props, searchFilter, user }) => {
   let paramPaging = '';
   let paramType = '';
   if (module === 'StockHolding') {
@@ -230,25 +235,27 @@ export const changePage = ({active, dispatch, module, props, searchFilter, user}
     paramPaging = 'PAGING_SP_DETAIL';
   }
   dispatch({ type: paramType, data: [] });
-  let arraySummary = ['StockHolding', 'purchaseOrder' , 'salesOrder','UserManagement' ]
-  if (arraySummary.includes(module)  ) {
-    getSummaryData({ dispatch, 
-                    active, 
-                    module, 
-                    props, 
-                    siteVal: searchFilter.siteVal,
-                    clientVal: searchFilter.clientVal,
-                    orderType: searchFilter.orderType,
-                    task: searchFilter.task,
-                    status: searchFilter.status,
-                    user })
+  let arraySummary = ['StockHolding', 'purchaseOrder', 'salesOrder', 'UserManagement']
+  if (arraySummary.includes(module)) {
+    getSummaryData({
+      dispatch,
+      active,
+      module,
+      props,
+      siteVal: searchFilter.siteVal,
+      clientVal: searchFilter.clientVal,
+      orderType: searchFilter.orderType,
+      task: searchFilter.task,
+      status: searchFilter.status,
+      user
+    })
   }
 
   if (module === 'StockHoldingForecast') {
-    getForescast({ dispatch, active, module , props});
+    getForescast({ dispatch, active, module, props });
   }
-  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail' , 'SalesOrdersDetail']
-  if (arrayDetail.includes(module)  ) {
-    getDetailData({ dispatch, active, module , props});
+  let arrayDetail = ['StockHoldingDetail', 'PurchaseOrdersDetail', 'SalesOrdersDetail']
+  if (arrayDetail.includes(module)) {
+    getDetailData({ dispatch, active, module, props });
   }
 }
